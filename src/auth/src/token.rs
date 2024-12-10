d93eec8a4166c6fc46610360ff14bebc4237d265
Carlos O'Ryan <coryan@google.com>
2024-12-10 14:20:07 -0500

    Address review comments

--- src/auth/src/token.rs
+++ src/auth/src/token.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/// Represents an authentication token.
+/// Represents an auth token.
 #[derive(Debug)]
 pub struct Token {
     /// The actual token string.
