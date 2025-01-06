9e20c913a85662ab0ca0d2616be25855489c79fc
Carlos O'Ryan <coryan@google.com>
2025-01-06 11:36:30 -0500

    docs: add "under development" disclaimers
    These are for hand-crafted files.

--- src/auth/src/lib.rs
+++ src/auth/src/lib.rs
@@ -14,6 +14,11 @@
 
 //! Google Cloud Client Libraries for Rust - Authentication Components
 //!
+//! **WARNING:** this crate is under active development. We expect multiple
+//! breaking changes in the upcoming releases. Testing is also incomplete, we do
+//! **not** recommend that you use this crate in production. We welcome feedback
+//! about the APIs, documentation, missing features, bugs, etc.
+//!
 //! This crate contains types and functions used to authenticate applications
 //! on Google Cloud.  The SDK clients consume an implementation of
 //! [credentials::Credential] and use these credentials to authenticate RPCs
