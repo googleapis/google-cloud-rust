5990939e8efbc59e020f374914aace32c199873f
Harkamal Jot Singh Kumar <harkamalj@google.com>
2025-01-08 09:44:28 +0000

    finish e2e testing

--- service_account_creds/src/main.rs
+++ service_account_creds/src/main.rs
@@ -1,9 +1,12 @@
 use gcp_sdk_auth::{credentials::service_account_credential, token::TokenProvider};  // Assuming the path is correct, replace with actual package and module name
 use tokio::runtime::Runtime;
+use rustls::crypto::CryptoProvider;
+
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let _ = CryptoProvider::install_default(rustls::crypto::aws_lc_rs::default_provider()); // Or another provider
     let rt = Runtime::new()?;
-    let mut token_provider = service_account_credential::ServiceAccountTokenProvider {
+    let token_provider = service_account_credential::ServiceAccountTokenProvider {
         file_path: "/usr/local/google/home/harkamalj/Desktop/sa3.json".to_string(),
     };
     println!("{:?}", rt.block_on(token_provider.get_token())?);
