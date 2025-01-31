googleapis/google-cloud-rust

commit 3a0083b823fc839b88d9cee3ac3c31eeb519bdac

refactor(generator): a package per language (#895)
I think this will make it easier to add additional languages and it
will (after another refactoring) cleanup the language specific function
names.

generator/internal/golang/golang.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package language
+package golang
 
 import (
 	"embed"
@@ -22,15 +22,26 @@ import (
 	"unicode"
 
 	"github.com/googleapis/google-cloud-rust/generator/internal/api"
+	"github.com/googleapis/google-cloud-rust/generator/internal/language"
 	"github.com/iancoleman/strcase"
 )
 
-//go:embed templates/go
+//go:embed templates
 var goTemplates embed.FS
 
 type goImport struct {
 	path string
 	name string
+}
+
+func Generate(model *api.API, outdir string, options map[string]string) error {
+	data, err := newGoTemplateData(model, options)
+	if err != nil {
+		return err
+	}
+	provider := goTemplatesProvider()
+	generatedFiles := language.WalkTemplatesDir(goTemplates, "templates/go")
+	return language.GenerateFromRoot(outdir, data, provider, generatedFiles)
 }
 
 func goLoadWellKnownTypes(s *api.APIState) {
@@ -91,7 +102,7 @@ func goAsQueryParameter(f *api.Field) string {
 	return fmt.Sprintf("req.%s.to_str()", strcase.ToLowerCamel(f.Name))
 }
 
-func goTemplatesProvider() templateProvider {
+func goTemplatesProvider() language.TemplateProvider {
 	return func(name string) (string, error) {
 		contents, err := goTemplates.ReadFile(name)
 		if err != nil {

generator/internal/golang/gotemplate.go
@@ -12,13 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package language
+package golang
 
 import (
 	"fmt"
 	"strings"
 
 	"github.com/googleapis/google-cloud-rust/generator/internal/api"
+	"github.com/googleapis/google-cloud-rust/generator/internal/language"
 	"github.com/googleapis/google-cloud-rust/generator/internal/license"
 	"github.com/iancoleman/strcase"
 )
@@ -177,7 +178,7 @@ func newGoTemplateData(model *api.API, options map[string]string) (*GoTemplateDa
 			}
 			return ""
 		}(),
-		Services: mapSlice(model.Services, func(s *api.Service) *GoService {
+		Services: language.MapSlice(model.Services, func(s *api.Service) *GoService {
 			return newGoService(s, model.State)
 		}),
 		Messages:  model.Messages,
@@ -197,11 +198,11 @@ func newGoTemplateData(model *api.API, options map[string]string) (*GoTemplateDa
 
 func newGoService(s *api.Service, state *api.APIState) *GoService {
 	// Some codecs skip some methods.
-	methods := filterSlice(s.Methods, func(m *api.Method) bool {
+	methods := language.FilterSlice(s.Methods, func(m *api.Method) bool {
 		return goGenerateMethod(m)
 	})
 	return &GoService{
-		Methods: mapSlice(methods, func(m *api.Method) *GoMethod {
+		Methods: language.MapSlice(methods, func(m *api.Method) *GoMethod {
 			return newGoMethod(m, s, state)
 		}),
 		NameToPascal:        goToPascal(s.Name),
@@ -223,10 +224,10 @@ func goAnnotateMessage(m *api.Message, state *api.APIState, importMap map[string
 	m.Codec = &goMessageAnnotation{
 		Name:           goMessageName(m, importMap),
 		QualifiedName:  goMessageName(m, importMap),
-		HasNestedTypes: hasNestedTypes(m),
+		HasNestedTypes: language.HasNestedTypes(m),
 		DocLines:       goFormatDocComments(m.Documentation, state),
 		SourceFQN:      strings.TrimPrefix(m.ID, "."),
-		BasicFields: filterSlice(m.Fields, func(s *api.Field) bool {
+		BasicFields: language.FilterSlice(m.Fields, func(s *api.Field) bool {
 			return !s.IsOneOf
 		}),
 	}
@@ -248,8 +249,8 @@ func newGoMethod(m *api.Method, s *api.Service, state *api.APIState) *GoMethod {
 		NameToCamel:         strcase.ToCamel(m.Name),
 		NameToPascal:        goToPascal(m.Name),
 		OutputTypeName:      goMethodInOutTypeName(m.OutputTypeID, state),
-		PathParams:          PathParams(m, state),
-		QueryParams:         QueryParams(m, state),
+		PathParams:          language.PathParams(m, state),
+		QueryParams:         language.QueryParams(m, state),
 		IsPageable:          m.IsPageable,
 		ServiceNameToPascal: goToPascal(s.Name),
 		InputTypeID:         m.InputTypeID,

generator/internal/golang/golang_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package language
+package golang
 
 import (
 	"testing"

generator/internal/golang/gotemplate_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package language
+package golang
 
 import (
 	"testing"

commit 1b8c68376c732503c2d5ee88deaf6182d46b8e1b

fix(generator/golang): fix testing and regressions
I broke the golang generator in #895: it found no templates and
therefore generated no code and nothing changed in the golden files.
Then I introduced regressions in #905 and #912. This restores the
testing and fixes the golden files.

generator/internal/golang/golang.go
@@ -40,7 +40,7 @@ func Generate(model *api.API, outdir string, options map[string]string) error {
 		return err
 	}
 	provider := templatesProvider()
-	generatedFiles := language.WalkTemplatesDir(goTemplates, "templates/go")
+	generatedFiles := language.WalkTemplatesDir(goTemplates, "templates")
 	return language.GenerateFromRoot(outdir, data, provider, generatedFiles)
 }
 

generator/internal/golang/gotemplate.go
@@ -42,7 +42,8 @@ type templateData struct {
 }
 
 type serviceAnnotations struct {
-	Name        string
+	FieldName   string
+	StructName  string
 	DocLines    []string
 	DefaultHost string
 }
@@ -58,15 +59,14 @@ type messageAnnotation struct {
 }
 
 type methodAnnotation struct {
-	Name                string
-	NameToPascal        string
-	DocLines            []string
-	PathParams          []*api.Field
-	QueryParams         []*api.Field
-	BodyAccessor        string
-	ServiceNameToPascal string
-	ServiceNameToCamel  string
-	OperationInfo       *operationInfo
+	Name              string
+	NameToPascal      string
+	DocLines          []string
+	PathParams        []*api.Field
+	QueryParams       []*api.Field
+	BodyAccessor      string
+	ServiceStructName string
+	OperationInfo     *operationInfo
 }
 
 type pathInfoAnnotation struct {
@@ -196,7 +196,8 @@ func annotateService(s *api.Service, state *api.APIState) {
 		annotateMethod(m, s, state)
 	}
 	ann := &serviceAnnotations{
-		Name:        toPascal(s.Name),
+		FieldName:   strcase.ToLowerCamel(s.Name),
+		StructName:  s.Name,
 		DocLines:    formatDocComments(s.Documentation, state),
 		DefaultHost: s.DefaultHost,
 	}
@@ -231,13 +232,13 @@ func annotateMethod(m *api.Method, s *api.Service, state *api.APIState) {
 	}
 	m.PathInfo.Codec = pathInfoAnnotation
 	annotation := &methodAnnotation{
-		BodyAccessor:        bodyAccessor(m),
-		DocLines:            formatDocComments(m.Documentation, state),
-		Name:                strcase.ToCamel(m.Name),
-		NameToPascal:        toPascal(m.Name),
-		PathParams:          language.PathParams(m, state),
-		QueryParams:         language.QueryParams(m, state),
-		ServiceNameToPascal: toPascal(s.Name),
+		BodyAccessor:      bodyAccessor(m),
+		DocLines:          formatDocComments(m.Documentation, state),
+		Name:              strcase.ToCamel(m.Name),
+		NameToPascal:      toPascal(m.Name),
+		PathParams:        language.PathParams(m, state),
+		QueryParams:       language.QueryParams(m, state),
+		ServiceStructName: s.Name,
 	}
 	if m.OperationInfo != nil {
 		annotation.OperationInfo = &operationInfo{

generator/internal/golang/templates/client.go.mustache
@@ -49,7 +49,7 @@ type Client struct {
     hc *http.Client
     opts *Options
     {{#Services}}
-    {{NameToCamel}} *{{ServiceName}}
+    {{Codec.FieldName}} *{{Codec.StructName}}
     {{/Services}}
 }
 
@@ -76,26 +76,26 @@ func (c *Client) token(ctx context.Context) (string, error) {
 }
 
 {{#Services}}
-{{#DocLines}}
+{{#Codec.DocLines}}
 // {{{.}}}
-{{/DocLines}}
-type {{ServiceName}} struct {
+{{/Codec.DocLines}}
+type {{Codec.StructName}} struct {
     client *Client
     baseURL   string
 }
 
-{{#DocLines}}
+{{#Codec.DocLines}}
 // {{{.}}}
-{{/DocLines}}
-func (c *Client) {{ServiceName}}() *{{ServiceName}}{
-    return &{{ServiceName}}{client: c, baseURL: defaultHost}
+{{/Codec.DocLines}}
+func (c *Client) {{Codec.StructName}}() *{{Codec.StructName}}{
+    return &{{Codec.StructName}}{client: c, baseURL: defaultHost}
 }
 
 {{#Methods}}
-{{#DocLines}}
+{{#Codec.DocLines}}
 // {{{.}}}
-{{/DocLines}}
-func (s *{{Codec.ServiceName}}) {{Codec.Name}}(ctx context.Context, req *{{InputTypeName}}) (*{{OutputType.Codec.Name}}, error) {
+{{/Codec.DocLines}}
+func (s *{{Codec.ServiceStructName}}) {{Codec.Name}}(ctx context.Context, req *{{InputType.Codec.Name}}) (*{{OutputType.Codec.Name}}, error) {
     out := new({{OutputType.Codec.Name}})
     {{#PathInfo.Codec.HasBody}}
     reqBody, err := json.Marshal(req{{Codec.BodyAccessor}})