82c126121e2f68fad3c431d91eaf56352abc644f
Carlos O'Ryan <coryan@google.com>
2025-02-04 09:31:31 -0500

    docs: bootstrap user guide
    Create an initial `mdbook` with a simple tutorial showing how to initialize a
    client.  Include the guide generation and testing of the code snippets *and*
    the included samples into the CI build.

---
+++ guide/samples/tests/list_resources.rs
@@ -0,0 +1,67 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use gcp_sdk_devtools_artifactregistry_v1::client::ArtifactRegistry;
+use std::env;
+
+type Result = std::result::Result<(), Box<dyn std::error::Error>>;
+
+async fn list_resources(project_id: &str) -> Result {
+    let client = ArtifactRegistry::new().await?;
+
+    let mut items = client
+        .list_repositories(format!("projects/{project_id}"))
+        .stream()
+        .await
+        .items();
+    while let Some(i) = items.next().await {
+        match i {
+            Ok(item) => {
+                println!("{item:?}");
+            }
+            Err(e) => {
+                println!("ERROR while iterating over the repositories {e}");
+                return Ok(());
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: list_artifacts <project-id>");
+        return Ok(());
+    }
+    let project_id = &args[1];
+
+    list_resources(project_id).await?;
+
+    Ok(())
+}
+
+#[cfg(all(test, feature = "run-integration-tests"))]
+mod test {
+    use super::*;
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn driver() -> Result {
+        let project_id = std::env::var("GOOGLE_CLOUD_PROJECT").unwrap();
+        list_resources(&project_id).await?;
+        Ok(())
+    }
+}
