d61302871d2fd8302f7fd6a7d76ead8505798795
dbolduc <dbolduc@google.com>
2025-02-19 11:27:41 -0500

    formatting; copyright year

--- src/auth/src/token_cache.rs
+++ src/auth/src/token_cache.rs
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -322,7 +322,7 @@ mod test {
         let tp = FakeTokenProvider::new(err);
 
         let cache = TokenCache::new(tp.clone());
-        
+
         // Spawn N tasks, all asking for a token at once.
         let tasks = (0..100)
             .map(|_| {
