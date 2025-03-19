7687131f6fbdb4768b570006288d835df548b776
Carlos O'Ryan <coryan@google.com>
2025-03-19 18:05:46 -0400

    refactor: rename `google-cloud-http-client`
    We decided to have a single `google-cloud-gax-internal` library.

--- src/lro/tests/fake_library/client.rs
+++ src/lro/tests/fake_library/client.rs
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 use super::*;
-use gclient::ReqwestClient;
+use gaxi::ReqwestClient;
 pub struct Client {
     // A sidekick-generated client contains a `Arc<dyn T>`. The code
     // in this test skips some layers of abstraction.
