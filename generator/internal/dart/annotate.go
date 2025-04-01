40f5497709699c8948912e672024149a2c3523c3
Devon Carew <devoncarew@google.com>
2025-04-01 14:14:09 -0700

    feat(generator/dart): generate google/iam/v1

--- generator/internal/dart/annotate.go
+++ generator/internal/dart/annotate.go
@@ -229,7 +229,7 @@ func (annotate *annotateModel) annotateModel(options map[string]string) (*modelA
 	ann := &modelAnnotations{
 		PackageName:    packageName(model, packageNameOverride),
 		PackageVersion: packageVersion,
-		MainFileName:   strcase.ToSnake(model.Name),
+		MainFileName:   model.Name,
 		HasServices:    len(model.Services) > 0,
 		CopyrightYear:  generationYear,
 		BoilerPlate: append(license.LicenseHeaderBulk(),
