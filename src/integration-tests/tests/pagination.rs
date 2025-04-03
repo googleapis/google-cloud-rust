d42c14f9dcfbf9e11bc6f799910b98cc08113bf2
dbolduc <dbolduc@google.com>
2025-04-03 17:03:55 -0400

    cleanup: pagination tests

--- src/integration-tests/tests/pagination.rs
+++ src/integration-tests/tests/pagination.rs
@@ -25,6 +25,121 @@ mod mocking {
     }
 
     #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+    async fn list_pages() -> TestResult {
+        let mut mock = MockSecretManagerService::new();
+        let mut seq = mockall::Sequence::new();
+        mock.expect_list_secrets()
+            .once()
+            .in_sequence(&mut seq)
+            .withf(|r, _| r.parent == "projects/test-project" && r.page_token.is_empty())
+            .return_once(|_, _| {
+                Ok(sm::model::ListSecretsResponse::default()
+                    .set_next_page_token("test-page-001")
+                    .set_secrets(make_secrets(3, 0)))
+            });
+        mock.expect_list_secrets()
+            .once()
+            .in_sequence(&mut seq)
+            .withf(|r, _| r.parent == "projects/test-project" && r.page_token == "test-page-001")
+            .return_once(|_, _| {
+                Ok(sm::model::ListSecretsResponse::default()
+                    .set_next_page_token("test-page-002")
+                    .set_secrets(make_secrets(3, 3)))
+            });
+        mock.expect_list_secrets()
+            .once()
+            .in_sequence(&mut seq)
+            .withf(|r, _| r.parent == "projects/test-project" && r.page_token == "test-page-002")
+            .return_once(|_, _| {
+                Ok(sm::model::ListSecretsResponse::default().set_secrets(make_secrets(3, 6)))
+            });
+
+        let client = sm::client::SecretManagerService::from_stub(mock);
+        let mut paginator = client
+            .list_secrets("projects/test-project")
+            .paginator()
+            .await;
+        let mut responses = Vec::new();
+        while let Some(response) = paginator.next().await {
+            responses.push(response?);
+        }
+
+        assert_eq!(
+            responses,
+            [
+                sm::model::ListSecretsResponse::default()
+                    .set_next_page_token("test-page-001")
+                    .set_secrets(make_secrets(3, 0)),
+                sm::model::ListSecretsResponse::default()
+                    .set_next_page_token("test-page-002")
+                    .set_secrets(make_secrets(3, 3)),
+                sm::model::ListSecretsResponse::default().set_secrets(make_secrets(3, 6)),
+            ]
+        );
+
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+    async fn list_pages_as_stream() -> TestResult {
+        use futures::stream::StreamExt;
+
+        let mut mock = MockSecretManagerService::new();
+        let mut seq = mockall::Sequence::new();
+        mock.expect_list_secrets()
+            .once()
+            .in_sequence(&mut seq)
+            .withf(|r, _| r.parent == "projects/test-project" && r.page_token.is_empty())
+            .return_once(|_, _| {
+                Ok(sm::model::ListSecretsResponse::default()
+                    .set_next_page_token("test-page-001")
+                    .set_secrets(make_secrets(3, 0)))
+            });
+        mock.expect_list_secrets()
+            .once()
+            .in_sequence(&mut seq)
+            .withf(|r, _| r.parent == "projects/test-project" && r.page_token == "test-page-001")
+            .return_once(|_, _| {
+                Ok(sm::model::ListSecretsResponse::default()
+                    .set_next_page_token("test-page-002")
+                    .set_secrets(make_secrets(3, 3)))
+            });
+        mock.expect_list_secrets()
+            .once()
+            .in_sequence(&mut seq)
+            .withf(|r, _| r.parent == "projects/test-project" && r.page_token == "test-page-002")
+            .return_once(|_, _| {
+                Ok(sm::model::ListSecretsResponse::default().set_secrets(make_secrets(3, 6)))
+            });
+
+        let client = sm::client::SecretManagerService::from_stub(mock);
+        let mut paginator = client
+            .list_secrets("projects/test-project")
+            .paginator()
+            .await
+            .into_stream();
+        let mut responses = Vec::new();
+        while let Some(response) = paginator.next().await {
+            responses.push(response?);
+        }
+
+        assert_eq!(
+            responses,
+            [
+                sm::model::ListSecretsResponse::default()
+                    .set_next_page_token("test-page-001")
+                    .set_secrets(make_secrets(3, 0)),
+                sm::model::ListSecretsResponse::default()
+                    .set_next_page_token("test-page-002")
+                    .set_secrets(make_secrets(3, 3)),
+                sm::model::ListSecretsResponse::default().set_secrets(make_secrets(3, 6)),
+            ]
+        );
+
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
     async fn list_items() -> TestResult {
         let mut mock = MockSecretManagerService::new();
         let mut seq = mockall::Sequence::new();
@@ -61,8 +176,8 @@ mod mocking {
             .await
             .items();
         let mut names = Vec::new();
-        while let Some(response) = paginator.next().await {
-            names.push(response?.name);
+        while let Some(secret) = paginator.next().await {
+            names.push(secret?.name);
         }
 
         assert_eq!(
@@ -116,8 +231,8 @@ mod mocking {
             .items()
             .into_stream();
         let mut names = Vec::new();
-        while let Some(response) = stream.next().await {
-            names.push(response?.name);
+        while let Some(secret) = stream.next().await {
+            names.push(secret?.name);
         }
 
         assert_eq!(
