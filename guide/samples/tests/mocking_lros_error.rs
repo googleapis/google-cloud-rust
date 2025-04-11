c61d3dcf133b1323137542140d8d5e0cce030d6c
dbolduc <dbolduc@google.com>
2025-04-11 13:26:32 -0400

    adopt gax::Response

--- guide/samples/tests/mocking_lros_error.rs
+++ guide/samples/tests/mocking_lros_error.rs
@@ -17,6 +17,7 @@
 // ANCHOR: all
 use gax::Result;
 use gax::error::Error;
+use gax::response::Response;
 use google_cloud_gax as gax;
 use google_cloud_longrunning as longrunning;
 use google_cloud_rpc as rpc;
@@ -102,24 +103,24 @@ mod test {
         #[derive(Debug)]
         Speech {}
         impl speech::stub::Speech for Speech {
-            async fn batch_recognize(&self, req: BatchRecognizeRequest, _options: gax::options::RequestOptions) -> Result<Operation>;
-            async fn get_operation(&self, req: GetOperationRequest, _options: gax::options::RequestOptions) -> Result<Operation>;
+            async fn batch_recognize(&self, req: BatchRecognizeRequest, _options: gax::options::RequestOptions) -> Result<Response<Operation>>;
+            async fn get_operation(&self, req: GetOperationRequest, _options: gax::options::RequestOptions) -> Result<Response<Operation>>;
         }
     }
 
-    fn make_partial_operation(progress: i32) -> Result<Operation> {
+    fn make_partial_operation(progress: i32) -> Result<Response<Operation>> {
         let metadata = OperationMetadata::new().set_progress_percent(progress);
         let any = wkt::Any::try_from(&metadata).map_err(Error::serde)?;
         let operation = Operation::new().set_metadata(Some(any));
-        Ok(operation)
+        Ok(Response::from(operation))
     }
 
     // ANCHOR: error-op
-    fn make_failed_operation(status: rpc::model::Status) -> Result<Operation> {
+    fn make_failed_operation(status: rpc::model::Status) -> Result<Response<Operation>> {
         let operation = Operation::new()
             .set_done(true)
             .set_result(OperationResult::Error(status.into()));
-        Ok(operation)
+        Ok(Response::from(operation))
     }
     // ANCHOR_END: error-op
 
