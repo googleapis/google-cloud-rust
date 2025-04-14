e1d14b3897da0f3931461b7c60f042bec7a3bd9e
Devon Carew <devoncarew@gmail.com>
2025-04-14 12:01:06 -0700

    feat(generator/dart): generate LRO Operations with generic type parameters

--- generator/dart/generated/google_cloud_longrunning/lib/longrunning.dart
+++ generator/dart/generated/google_cloud_longrunning/lib/longrunning.dart
@@ -34,6 +34,8 @@ import 'package:google_cloud_protobuf/protobuf.dart';
 import 'package:google_cloud_rpc/rpc.dart';
 import 'package:http/http.dart' as http;
 
+part 'src/longrunning.p.dart';
+
 /// Manages long-running operations with an API service.
 ///
 /// When an API method normally takes long time to complete, it can be designed
@@ -98,79 +100,6 @@ class Operations {
   ///
   /// Once [close] is called, no other methods should be called.
   void close() => _client.close();
-}
-
-/// This resource represents a long-running operation that is the result of a
-/// network API call.
-class Operation extends Message {
-  static const String fullyQualifiedName = 'google.longrunning.Operation';
-
-  /// The server-assigned name, which is only unique within the same service that
-  /// originally returns it. If you use the default HTTP mapping, the
-  /// `name` should be a resource name ending with `operations/{unique_id}`.
-  final String? name;
-
-  /// Service-specific metadata associated with the operation.  It typically
-  /// contains progress information and common metadata such as create time.
-  /// Some services might not provide such metadata.  Any method that returns a
-  /// long-running operation should document the metadata type, if any.
-  final Any? metadata;
-
-  /// If the value is `false`, it means the operation is still in progress.
-  /// If `true`, the operation is completed, and either `error` or `response` is
-  /// available.
-  final bool? done;
-
-  /// The error result of the operation in case of failure or cancellation.
-  final Status? error;
-
-  /// The normal, successful response of the operation.  If the original
-  /// method returns no data on success, such as `Delete`, the response is
-  /// `google.protobuf.Empty`.  If the original method is standard
-  /// `Get`/`Create`/`Update`, the response should be the resource.  For other
-  /// methods, the response should have the type `XxxResponse`, where `Xxx`
-  /// is the original method name.  For example, if the original method name
-  /// is `TakeSnapshot()`, the inferred response type is
-  /// `TakeSnapshotResponse`.
-  final Any? response;
-
-  Operation({
-    this.name,
-    this.metadata,
-    this.done,
-    this.error,
-    this.response,
-  }) : super(fullyQualifiedName);
-
-  factory Operation.fromJson(Map<String, dynamic> json) {
-    return Operation(
-      name: json['name'],
-      metadata: decode(json['metadata'], Any.fromJson),
-      done: json['done'],
-      error: decode(json['error'], Status.fromJson),
-      response: decode(json['response'], Any.fromJson),
-    );
-  }
-
-  @override
-  Object toJson() {
-    return {
-      if (name != null) 'name': name,
-      if (metadata != null) 'metadata': metadata!.toJson(),
-      if (done != null) 'done': done,
-      if (error != null) 'error': error!.toJson(),
-      if (response != null) 'response': response!.toJson(),
-    };
-  }
-
-  @override
-  String toString() {
-    final contents = [
-      if (name != null) 'name=$name',
-      if (done != null) 'done=$done',
-    ].join(',');
-    return 'Operation($contents)';
-  }
 }
 
 /// The request message for
