0923577354434438168c87a1bbbd6fd000a1474b
Carlos O'Ryan <coryan@google.com>
2025-04-15 15:01:05 +0000

    impl: create scaffolds for hand-crafted libraries (#1802)

---
+++ src/bigquery/README.md
@@ -0,0 +1,10 @@
+# Google Cloud Client Libraries for Rust - BigQuery
+
+This crate implements the [BigQuery] client library.
+
+**WARNING:** this crate is under active development. We expect multiple breaking
+changes in the upcoming releases. Testing is also incomplete, we do **not**
+recommend that you use this crate in production. We welcome feedback about the
+APIs, documentation, missing features, bugs, etc.
+
+[bigquery]: https://cloud.google.com/bigquery
---
+++ src/storage/src/lib.rs
@@ -0,0 +1,25 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Google Cloud Client Libraries for Rust - Storage
+//!
+//! **WARNING:** this crate is under active development. We expect multiple
+//! breaking changes in the upcoming releases. Testing is also incomplete, we do
+//! **not** recommend that you use this crate in production. We welcome feedback
+//! about the APIs, documentation, missing features, bugs, etc.
+//!
+//! This crate contains traits, types, and functions to interact with
+//! [Storage].
+//!
+//! [storage]: https://cloud.google.com/storage
