cdfb9c06e5660d9be110300b40866eca0056dccf
Carlos O'Ryan <coryan@google.com>
2025-04-16 20:34:09 +0000

    feat(storage): add generated code (#1821)
    Add the generated code and minimum supporting files to compile it.
    Disable a number of warnings for now. Export `client`, `builder` and
    other modules because they are needed for `cargo doc` to pass.

--- src/storage/src/lib.rs
+++ src/storage/src/lib.rs
@@ -23,3 +23,60 @@
 //! [Storage].
 //!
 //! [storage]: https://cloud.google.com/storage
+
+pub use gax::Result;
+pub use gax::error::Error;
+#[allow(dead_code)]
+// TODO(#1813) - fix the broken link to [here].
+#[allow(rustdoc::broken_intra_doc_links)]
+// TODO(#1813) - fix x-goog-request-params and this is not needed
+#[allow(clippy::op_ref)]
+#[allow(clippy::needless_borrow)]
+pub(crate) mod generated;
+
+pub use generated::gapic::builder;
+pub use generated::gapic::client;
+pub use generated::gapic::model;
+
+pub(crate) mod google {
+    pub mod iam {
+        pub mod v1 {
+            include!("generated/protos/storage/google.iam.v1.rs");
+        }
+    }
+    pub mod r#type {
+        include!("generated/protos/storage/google.r#type.rs");
+        include!("generated/convert/type/convert.rs");
+    }
+    pub mod rpc {
+        include!("generated/protos/storage/google.rpc.rs");
+    }
+    pub mod storage {
+        #[allow(deprecated)]
+        pub mod v2 {
+            include!("generated/protos/storage/google.storage.v2.rs");
+            include!("generated/convert/storage/convert.rs");
+        }
+    }
+}
+
+impl gaxi::prost::Convert<google::rpc::Status> for rpc::model::Status {
+    fn cnv(self) -> google::rpc::Status {
+        google::rpc::Status {
+            code: self.code.cnv(),
+            message: self.message.cnv(),
+            // TODO(#...) - detail with the error details
+            ..Default::default()
+        }
+    }
+}
+
+impl gaxi::prost::Convert<rpc::model::Status> for google::rpc::Status {
+    fn cnv(self) -> rpc::model::Status {
+        rpc::model::Status::new()
+            .set_code(self.code)
+            .set_message(self.message)
+        // TODO(#...) - detail with the error details
+        // .set_details(self.details.into_iter().filter_map(any_from_prost))
+    }
+}
