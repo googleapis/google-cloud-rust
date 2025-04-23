ea08fa2699062912e86efd1a3cc74de5a5c39366
Devon Carew <devoncarew@google.com>
2025-04-23 10:14:52 -0700

    feat(generator/dart): bump to the latest API versions

--- generator/dart/generated/google_cloud_rpc/lib/rpc.dart
+++ generator/dart/generated/google_cloud_rpc/lib/rpc.dart
@@ -248,15 +248,92 @@ class QuotaFailure_Violation extends ProtoMessage {
   /// exceeded".
   final String? description;
 
+  /// The API Service from which the `QuotaFailure.Violation` orginates. In
+  /// some cases, Quota issues originate from an API Service other than the one
+  /// that was called. In other words, a dependency of the called API Service
+  /// could be the cause of the `QuotaFailure`, and this field would have the
+  /// dependency API service name.
+  ///
+  /// For example, if the called API is Kubernetes Engine API
+  /// (container.googleapis.com), and a quota violation occurs in the
+  /// Kubernetes Engine API itself, this field would be
+  /// "container.googleapis.com". On the other hand, if the quota violation
+  /// occurs when the Kubernetes Engine API creates VMs in the Compute Engine
+  /// API (compute.googleapis.com), this field would be
+  /// "compute.googleapis.com".
+  final String? apiService;
+
+  /// The metric of the violated quota. A quota metric is a named counter to
+  /// measure usage, such as API requests or CPUs. When an activity occurs in a
+  /// service, such as Virtual Machine allocation, one or more quota metrics
+  /// may be affected.
+  ///
+  /// For example, "compute.googleapis.com/cpus_per_vm_family",
+  /// "storage.googleapis.com/internet_egress_bandwidth".
+  final String? quotaMetric;
+
+  /// The id of the violated quota. Also know as "limit name", this is the
+  /// unique identifier of a quota in the context of an API service.
+  ///
+  /// For example, "CPUS-PER-VM-FAMILY-per-project-region".
+  final String? quotaId;
+
+  /// The dimensions of the violated quota. Every non-global quota is enforced
+  /// on a set of dimensions. While quota metric defines what to count, the
+  /// dimensions specify for what aspects the counter should be increased.
+  ///
+  /// For example, the quota "CPUs per region per VM family" enforces a limit
+  /// on the metric "compute.googleapis.com/cpus_per_vm_family" on dimensions
+  /// "region" and "vm_family". And if the violation occurred in region
+  /// "us-central1" and for VM family "n1", the quota_dimensions would be,
+  ///
+  /// {
+  ///   "region": "us-central1",
+  ///   "vm_family": "n1",
+  /// }
+  ///
+  /// When a quota is enforced globally, the quota_dimensions would always be
+  /// empty.
+  final Map<String, String>? quotaDimensions;
+
+  /// The enforced quota value at the time of the `QuotaFailure`.
+  ///
+  /// For example, if the enforced quota value at the time of the
+  /// `QuotaFailure` on the number of CPUs is "10", then the value of this
+  /// field would reflect this quantity.
+  final int? quotaValue;
+
+  /// The new quota value being rolled out at the time of the violation. At the
+  /// completion of the rollout, this value will be enforced in place of
+  /// quota_value. If no rollout is in progress at the time of the violation,
+  /// this field is not set.
+  ///
+  /// For example, if at the time of the violation a rollout is in progress
+  /// changing the number of CPUs quota from 10 to 20, 20 would be the value of
+  /// this field.
+  final int? futureQuotaValue;
+
   QuotaFailure_Violation({
     this.subject,
     this.description,
+    this.apiService,
+    this.quotaMetric,
+    this.quotaId,
+    this.quotaDimensions,
+    this.quotaValue,
+    this.futureQuotaValue,
   }) : super(fullyQualifiedName);
 
   factory QuotaFailure_Violation.fromJson(Map<String, dynamic> json) {
     return QuotaFailure_Violation(
       subject: json['subject'],
       description: json['description'],
+      apiService: json['apiService'],
+      quotaMetric: json['quotaMetric'],
+      quotaId: json['quotaId'],
+      quotaDimensions: decodeMap(json['quotaDimensions']),
+      quotaValue: decodeInt64(json['quotaValue']),
+      futureQuotaValue: decodeInt64(json['futureQuotaValue']),
     );
   }
 
@@ -265,6 +342,13 @@ class QuotaFailure_Violation extends ProtoMessage {
     return {
       if (subject != null) 'subject': subject,
       if (description != null) 'description': description,
+      if (apiService != null) 'apiService': apiService,
+      if (quotaMetric != null) 'quotaMetric': quotaMetric,
+      if (quotaId != null) 'quotaId': quotaId,
+      if (quotaDimensions != null) 'quotaDimensions': quotaDimensions,
+      if (quotaValue != null) 'quotaValue': encodeInt64(quotaValue),
+      if (futureQuotaValue != null)
+        'futureQuotaValue': encodeInt64(futureQuotaValue),
     };
   }
 
@@ -273,6 +357,11 @@ class QuotaFailure_Violation extends ProtoMessage {
     final contents = [
       if (subject != null) 'subject=$subject',
       if (description != null) 'description=$description',
+      if (apiService != null) 'apiService=$apiService',
+      if (quotaMetric != null) 'quotaMetric=$quotaMetric',
+      if (quotaId != null) 'quotaId=$quotaId',
+      if (quotaValue != null) 'quotaValue=$quotaValue',
+      if (futureQuotaValue != null) 'futureQuotaValue=$futureQuotaValue',
     ].join(',');
     return 'Violation($contents)';
   }
