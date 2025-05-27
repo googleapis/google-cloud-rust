b071b813d0774f7d2dda671c9588f441e6c88ca7
Suzy Mueller <suzmue@golang.org>
2025-05-27 13:21:40 -0700

    impl: cleanup fully qualified Option, Into and String

--- src/storage/src/client.rs
+++ src/storage/src/client.rs
@@ -292,31 +292,31 @@ impl<'a> ReadObject<'a> {
     }
 
     /// Sets the value of [bucket][control::model::ReadObjectRequest::bucket].
-    pub fn set_bucket<T: std::convert::Into<std::string::String>>(mut self, v: T) -> Self {
+    pub fn set_bucket<T: Into<String>>(mut self, v: T) -> Self {
         self.request.bucket = v.into();
         self
     }
 
     /// Sets the value of [object][control::model::ReadObjectRequest::object].
-    pub fn set_object<T: std::convert::Into<std::string::String>>(mut self, v: T) -> Self {
+    pub fn set_object<T: Into<String>>(mut self, v: T) -> Self {
         self.request.object = v.into();
         self
     }
 
     /// Sets the value of [generation][control::model::ReadObjectRequest::generation].
-    pub fn set_generation<T: std::convert::Into<i64>>(mut self, v: T) -> Self {
+    pub fn set_generation<T: Into<i64>>(mut self, v: T) -> Self {
         self.request.generation = v.into();
         self
     }
 
     /// Sets the value of [read_offset][control::model::ReadObjectRequest::read_offset].
-    pub fn set_read_offset<T: std::convert::Into<i64>>(mut self, v: T) -> Self {
+    pub fn set_read_offset<T: Into<i64>>(mut self, v: T) -> Self {
         self.request.read_offset = v.into();
         self
     }
 
     /// Sets the value of [read_limit][control::model::ReadObjectRequest::read_limit].
-    pub fn set_read_limit<T: std::convert::Into<i64>>(mut self, v: T) -> Self {
+    pub fn set_read_limit<T: Into<i64>>(mut self, v: T) -> Self {
         self.request.read_limit = v.into();
         self
     }
@@ -324,16 +324,16 @@ impl<'a> ReadObject<'a> {
     /// Sets the value of [if_generation_match][control::model::ReadObjectRequest::if_generation_match].
     pub fn set_if_generation_match<T>(mut self, v: T) -> Self
     where
-        T: std::convert::Into<i64>,
+        T: Into<i64>,
     {
-        self.request.if_generation_match = std::option::Option::Some(v.into());
+        self.request.if_generation_match = Option::Some(v.into());
         self
     }
 
     /// Sets or clears the value of [if_generation_match][control::model::ReadObjectRequest::if_generation_match].
-    pub fn set_or_clear_if_generation_match<T>(mut self, v: std::option::Option<T>) -> Self
+    pub fn set_or_clear_if_generation_match<T>(mut self, v: Option<T>) -> Self
     where
-        T: std::convert::Into<i64>,
+        T: Into<i64>,
     {
         self.request.if_generation_match = v.map(|x| x.into());
         self
@@ -342,16 +342,16 @@ impl<'a> ReadObject<'a> {
     /// Sets the value of [if_generation_not_match][control::model::ReadObjectRequest::if_generation_not_match].
     pub fn set_if_generation_not_match<T>(mut self, v: T) -> Self
     where
-        T: std::convert::Into<i64>,
+        T: Into<i64>,
     {
-        self.request.if_generation_not_match = std::option::Option::Some(v.into());
+        self.request.if_generation_not_match = Option::Some(v.into());
         self
     }
 
     /// Sets or clears the value of [if_generation_not_match][control::model::ReadObjectRequest::if_generation_not_match].
-    pub fn set_or_clear_if_generation_not_match<T>(mut self, v: std::option::Option<T>) -> Self
+    pub fn set_or_clear_if_generation_not_match<T>(mut self, v: Option<T>) -> Self
     where
-        T: std::convert::Into<i64>,
+        T: Into<i64>,
     {
         self.request.if_generation_not_match = v.map(|x| x.into());
         self
@@ -360,16 +360,16 @@ impl<'a> ReadObject<'a> {
     /// Sets the value of [if_metageneration_match][control::model::ReadObjectRequest::if_metageneration_match].
     pub fn set_if_metageneration_match<T>(mut self, v: T) -> Self
     where
-        T: std::convert::Into<i64>,
+        T: Into<i64>,
     {
-        self.request.if_metageneration_match = std::option::Option::Some(v.into());
+        self.request.if_metageneration_match = Option::Some(v.into());
         self
     }
 
     /// Sets or clears the value of [if_metageneration_match][control::model::ReadObjectRequest::if_metageneration_match].
-    pub fn set_or_clear_if_metageneration_match<T>(mut self, v: std::option::Option<T>) -> Self
+    pub fn set_or_clear_if_metageneration_match<T>(mut self, v: Option<T>) -> Self
     where
-        T: std::convert::Into<i64>,
+        T: Into<i64>,
     {
         self.request.if_metageneration_match = v.map(|x| x.into());
         self
@@ -378,16 +378,16 @@ impl<'a> ReadObject<'a> {
     /// Sets the value of [if_metageneration_not_match][control::model::ReadObjectRequest::if_metageneration_not_match].
     pub fn set_if_metageneration_not_match<T>(mut self, v: T) -> Self
     where
-        T: std::convert::Into<i64>,
+        T: Into<i64>,
     {
-        self.request.if_metageneration_not_match = std::option::Option::Some(v.into());
+        self.request.if_metageneration_not_match = Option::Some(v.into());
         self
     }
 
     /// Sets or clears the value of [if_metageneration_not_match][control::model::ReadObjectRequest::if_metageneration_not_match].
-    pub fn set_or_clear_if_metageneration_not_match<T>(mut self, v: std::option::Option<T>) -> Self
+    pub fn set_or_clear_if_metageneration_not_match<T>(mut self, v: Option<T>) -> Self
     where
-        T: std::convert::Into<i64>,
+        T: Into<i64>,
     {
         self.request.if_metageneration_not_match = v.map(|x| x.into());
         self
@@ -396,16 +396,16 @@ impl<'a> ReadObject<'a> {
     /// Sets the value of [common_object_request_params][control::model::ReadObjectRequest::common_object_request_params].
     pub fn set_common_object_request_params<T>(mut self, v: T) -> Self
     where
-        T: std::convert::Into<control::model::CommonObjectRequestParams>,
+        T: Into<control::model::CommonObjectRequestParams>,
     {
-        self.request.common_object_request_params = std::option::Option::Some(v.into());
+        self.request.common_object_request_params = Option::Some(v.into());
         self
     }
 
     /// Sets or clears the value of [common_object_request_params][control::model::ReadObjectRequest::common_object_request_params].
-    pub fn set_or_clear_common_object_request_params<T>(mut self, v: std::option::Option<T>) -> Self
+    pub fn set_or_clear_common_object_request_params<T>(mut self, v: Option<T>) -> Self
     where
-        T: std::convert::Into<control::model::CommonObjectRequestParams>,
+        T: Into<control::model::CommonObjectRequestParams>,
     {
         self.request.common_object_request_params = v.map(|x| x.into());
         self
@@ -414,16 +414,16 @@ impl<'a> ReadObject<'a> {
     /// Sets the value of [read_mask][control::model::ReadObjectRequest::read_mask].
     pub fn set_read_mask<T>(mut self, v: T) -> Self
     where
-        T: std::convert::Into<wkt::FieldMask>,
+        T: Into<wkt::FieldMask>,
     {
-        self.request.read_mask = std::option::Option::Some(v.into());
+        self.request.read_mask = Option::Some(v.into());
         self
     }
 
     /// Sets or clears the value of [read_mask][control::model::ReadObjectRequest::read_mask].
-    pub fn set_or_clear_read_mask<T>(mut self, v: std::option::Option<T>) -> Self
+    pub fn set_or_clear_read_mask<T>(mut self, v: Option<T>) -> Self
     where
-        T: std::convert::Into<wkt::FieldMask>,
+        T: Into<wkt::FieldMask>,
     {
         self.request.read_mask = v.map(|x| x.into());
         self
@@ -513,7 +513,7 @@ mod v1 {
         acl: Vec<ObjectAccessControl>,
         owner: Option<Owner>,
         customer_encryption: Option<CustomerEncryption>,
-        metadata: std::collections::HashMap<std::string::String, std::string::String>,
+        metadata: std::collections::HashMap<String, String>,
         #[serde_as(as = "Option<Crc32c>")]
         crc32c: Option<u32>,
         #[serde_as(as = "serde_with::base64::Base64")]
