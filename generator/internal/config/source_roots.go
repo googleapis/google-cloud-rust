92051544862614772c2fa5c5b7a686249e5721d6
Carlos O'Ryan <coryan@google.com>
2025-05-30 14:52:30 -0400

    Always add the -root suffix

--- generator/internal/config/source_roots.go
+++ generator/internal/config/source_roots.go
@@ -23,11 +23,7 @@ func SourceRoots(options map[string]string) []string {
 	if opt, ok := options["roots"]; ok {
 		var roots []string
 		for _, name := range strings.Split(opt, ",") {
-			if strings.HasSuffix(name, "-root") {
-				roots = append(roots, name)
-			} else {
-				roots = append(roots, fmt.Sprintf("%s-root", name))
-			}
+			roots = append(roots, fmt.Sprintf("%s-root", name))
 		}
 		return roots
 	}
