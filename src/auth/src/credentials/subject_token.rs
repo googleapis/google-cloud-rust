e9494d62c992bb4ad35e58b8a86611613df36e2e
Harkamal Jot Singh Kumar <harkamalj@google.com>
2025-06-21 03:01:44 +0000

    add doc comments

--- src/auth/src/credentials/subject_token.rs
+++ src/auth/src/credentials/subject_token.rs
@@ -14,26 +14,64 @@
 
 use crate::credentials::errors::SubjectTokenProviderError;
 
+/// A builder for [SubjectToken] instances.
+///
+/// # Example
+/// ```
+/// # use google_cloud_auth::credentials::subjet_token::Builder;
+/// let subject_token = Builder::new("test-token")
+///     .build()?;
+///
 pub struct Builder {
     token: String,
 }
 
 impl Builder {
+    /// Creates a new Builder using the string token.
     pub fn new(token: String) -> Self {
         Self { token }
     }
 
+    /// Returns a [SubjectToken] instance with the configured token.
     pub fn build(self) -> SubjectToken {
         SubjectToken { token: self.token }
     }
 }
 
+/// Represents a third-party subject token used for authentication.
+///
+/// This token is typically obtained from an external identity provider and is
+/// exchanged for a Google Cloud access token via the Security Token Service (STS)
+///
+/// [SubjectToken] should be constructed using it's corresponding [Builder].
+///
+/// # Example
+///
+/// ```
+/// # # use google_cloud_auth::credentials::subjet_token::Builder;
+///
+/// let token_value = "my-secret-token".to_string();
+/// let subject_token = Builder::new(token_value).build();
+///
+/// ```
 #[derive(Debug)]
 pub struct SubjectToken {
     pub(crate) token: String,
 }
 
+/// A trait for providing a third-party subject token.
+/// TODO(#2254): Add documentation and example after implementation.
 pub trait SubjectTokenProvider: std::fmt::Debug + Send + Sync {
+    /// The error type that can be returned by this provider.
+    ///
+    /// The error must implement the [`SubjectTokenProviderError`] trait to allow the
+    /// authentication client to know whether the error is transient and can be retried.
     type Error: SubjectTokenProviderError;
+    /// Asynchronously fetches the third-party subject token.
+    ///
+    /// # Returns
+    ///
+    /// - `Ok(SubjectToken)`: On a successful token fetch.
+    /// - `Err(Self::Error)`: On a failure, returning the custom error type.
     fn subject_token(&self) -> impl Future<Output = Result<SubjectToken, Self::Error>> + Send;
 }
