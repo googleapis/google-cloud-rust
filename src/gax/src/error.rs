593a8729f8265500fb92858830acf5ffdadc5af2
Darren Bolduc <dbolduc@google.com>
2025-06-22 08:49:37 -0700

    feat(gax): introduce BindingError (#2487)

--- src/gax/src/error.rs
+++ src/gax/src/error.rs
@@ -17,6 +17,13 @@ pub use core_error::*;
 mod credentials;
 pub use credentials::CredentialsError;
 
+/// Errors and error details related to local path validation.
+///
+/// These errors occur when required fields in a request are either missing, or
+/// are present, but in an invalid format. The client fails these requests
+/// locally because it does not know how to send such requests.
+pub mod binding;
+
 /// Errors and error details returned by Service RPCs.
 ///
 /// The Google Cloud Client Libraries for Rust distinguishes between errors detected while
