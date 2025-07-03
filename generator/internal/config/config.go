c6ac50c21249d7ee08aefde020486b6e8a1403b9
Julie Qiu <julie@golang.org>
2025-07-03 18:25:40 -0400

    rename gcloud to gcloudyaml

--- generator/internal/config/config.go
+++ generator/internal/config/config.go
@@ -26,7 +26,7 @@ import (
 	"path"
 	"strings"
 
-	"github.com/googleapis/google-cloud-rust/generator/internal/config/gcloud"
+	"github.com/googleapis/google-cloud-rust/generator/internal/config/gcloudyaml"
 	"github.com/googleapis/google-cloud-rust/generator/internal/license"
 	toml "github.com/pelletier/go-toml/v2"
 )
@@ -59,7 +59,7 @@ type Config struct {
 
 	// Gcloud is used to pass data into gcloud.Generate. It does not use the
 	// normal .sidekick.toml file, but instead reads a gcloud.yaml file.
-	Gcloud *gcloud.Config
+	Gcloud *gcloudyaml.Config
 }
 
 // Configuration parameters that affect Parsers and Codecs, including the
