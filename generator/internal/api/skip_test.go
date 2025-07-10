6071c973fc85a4afae50dd49ca240c881949221a
dbolduc <dbolduc@google.com>
2025-07-10 00:30:43 -0400

    impl(sidekick): introduce `included-ids`

--- generator/internal/api/skip_test.go
+++ generator/internal/api/skip_test.go
@@ -224,3 +224,129 @@ func TestSkipMethods(t *testing.T) {
 		t.Errorf("mismatch in methods (-want, +got)\n:%s", diff)
 	}
 }
+
+func TestIncludeNestedEnums(t *testing.T) {
+	e0 := &Enum{
+		Name:    "Enum0",
+		Package: "test",
+		ID:      ".test.Message.Enum0",
+	}
+	e1 := &Enum{
+		Name:    "Enum1",
+		Package: "test",
+		ID:      ".test.Message.Enum1",
+	}
+	e2 := &Enum{
+		Name:    "Enum2",
+		Package: "test",
+		ID:      ".test.Message.Enum2",
+	}
+	m := &Message{
+		Name:    "Message",
+		Package: "test",
+		ID:      ".test.Message",
+	}
+	model := NewTestAPI([]*Message{m}, []*Enum{e0, e1, e2}, []*Service{})
+	CrossReference(model)
+	SkipModelElements(model, map[string]string{
+		"included-ids": ".test.Message.Enum0",
+	})
+
+	want := []*Enum{e0}
+	if diff := cmp.Diff(want, m.Enums, cmpopts.IgnoreFields(Message{}, "Enums")); diff != "" {
+		t.Errorf("mismatch in enums (-want, +got)\n:%s", diff)
+	}
+}
+
+func TestIncludeNestedMessages(t *testing.T) {
+	m0 := &Message{
+		Name:    "Message0",
+		Package: "test",
+		ID:      ".test.Message2.Message0",
+	}
+	m1 := &Message{
+		Name:    "Message1",
+		Package: "test",
+		ID:      ".test.Message2.Message1",
+	}
+	m2 := &Message{
+		Name:    "Message2",
+		Package: "test",
+		ID:      ".test.Message2",
+	}
+	model := NewTestAPI([]*Message{m0, m1, m2}, []*Enum{}, []*Service{})
+	CrossReference(model)
+	SkipModelElements(model, map[string]string{
+		"included-ids": ".test.Message2.Message0",
+	})
+	want := []*Message{m0}
+	if diff := cmp.Diff(want, m2.Messages, cmpopts.IgnoreFields(Message{}, "Messages")); diff != "" {
+		t.Errorf("mismatch in messages (-want, +got)\n:%s", diff)
+	}
+}
+
+func TestIncludeMethods(t *testing.T) {
+	m := &Message{
+		Name: "Empty",
+		ID:   ".test.Empty",
+	}
+	s0 := &Service{
+		Name:    "Service0",
+		Package: "test",
+		ID:      ".test.Service0",
+	}
+	s1 := &Service{
+		Name:    "Service1",
+		Package: "test",
+		ID:      ".test.Service1",
+		Methods: []*Method{
+			{
+				Name:         "Method0",
+				ID:           ".test.Service1.Method0",
+				InputTypeID:  ".test.Empty",
+				OutputTypeID: ".test.Empty",
+			},
+			{
+				Name:         "Method1",
+				ID:           ".test.Service1.Method1",
+				InputTypeID:  ".test.Empty",
+				OutputTypeID: ".test.Empty",
+			},
+			{
+				Name:         "Method2",
+				ID:           ".test.Service1.Method2",
+				InputTypeID:  ".test.Empty",
+				OutputTypeID: ".test.Empty",
+			},
+		},
+	}
+	s2 := &Service{
+		Name:    "Service2",
+		Package: "test",
+		ID:      ".test.Service2",
+	}
+	model := NewTestAPI([]*Message{m}, []*Enum{}, []*Service{s0, s1, s2})
+	CrossReference(model)
+	SkipModelElements(model, map[string]string{
+		"included-ids": ".test.Service1.Method1,.test.Service1.Method2",
+	})
+
+	wantServices := []*Service{s1}
+	if diff := cmp.Diff(wantServices, model.Services, cmpopts.IgnoreFields(Method{}, "Model"), cmpopts.IgnoreFields(Service{}, "Model")); diff != "" {
+		t.Errorf("mismatch in services (-want, +got)\n:%s", diff)
+	}
+
+	wantMethods := []*Method{
+		{
+			Name: "Method1",
+			ID:   ".test.Service1.Method1",
+		},
+		{
+			Name: "Method2",
+			ID:   ".test.Service1.Method2",
+		},
+	}
+	if diff := cmp.Diff(wantMethods, s1.Methods, cmpopts.IgnoreFields(Method{}, "Model", "Service", "InputType", "OutputType", "InputTypeID", "OutputTypeID")); diff != "" {
+		t.Errorf("mismatch in methods (-want, +got)\n:%s", diff)
+	}
+}
