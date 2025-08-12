6c0ed460f7e25d1070514e3b6c2c22fb8c987955
Carlos O'Ryan <coryan@google.com>
2025-08-12 11:48:18 -0400

    fix(storage): fewer errors abort uploads
    Only errors that come from the user should abort an upload. Other errors
    are I/O errors and should allow the upload to continue (subject to the
    retry or resume policies).

--- src/storage/src/storage/perform_upload/unbuffered.rs
+++ src/storage/src/storage/perform_upload/unbuffered.rs
@@ -99,7 +99,7 @@ where
             .map_err(Error::ser)?;
         let payload = self.payload_to_body().await?;
         let builder = builder.body(payload);
-        let response = builder.send().await.map_err(super::send_err)?;
+        let response = builder.send().await.map_err(Self::send_err)?;
         let object = self::handle_object_response(response).await?;
         self.validate_response_object(object).await
     }
@@ -126,7 +126,7 @@ where
 
     async fn single_shot_attempt(&self) -> Result<Object> {
         let builder = self.single_shot_builder().await?;
-        let response = builder.send().await.map_err(super::send_err)?;
+        let response = builder.send().await.map_err(Self::send_err)?;
         let object = super::handle_object_response(response).await?;
         self.validate_response_object(object).await
     }
