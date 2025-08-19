ef36bafd52ab6684010a00bc6067e50b92e90a57
Carlos O'Ryan <coryan@google.com>
2025-08-19 11:27:20 -0400

    Address review comments

--- src/storage/src/storage/perform_upload/tests/preconditions.rs
+++ src/storage/src/storage/perform_upload/tests/preconditions.rs
@@ -14,22 +14,6 @@
 
 //! Verify the client library correctly sets preconditions on uploads.
 
-// Copyright 2025 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-//! Verify the client library correctly detects mismatched checksums on uploads.
-
 use super::*;
 use crate::storage::streaming_source::BytesSource;
 use gax::retry_policy::{Aip194Strict, RetryPolicyExt};
@@ -157,9 +141,6 @@ fn resumable_server() -> Server {
 }
 
 fn body() -> Value {
-    // The magic strings can be regenerated using:
-    //     echo -n 'how vexingly quick daft zebras jump' > vexing.txt
-    //     gcloud hash vexing.txt
     json!({
         "bucket": "/projects/_/buckets/test-bucket",
         "name": "test-object",
