d94bf03ececa557acb756a547d8ca5d7c37c115b
Carlos O'Ryan <coryan@google.com>
2025-08-22 09:36:50 -0400

    Gemini's output

---
+++ src/storage/examples/src/remove_retention_policy.rs
@@ -0,0 +1,37 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START storage_remove_retention_policy]
+use google_cloud_storage::client::StorageControl;
+use google_cloud_storage::model::bucket::RetentionPolicy;
+use google_cloud_wkt::FieldMask;
+
+pub async fn sample(client: &StorageControl, bucket_id: &str) -> anyhow::Result<()> {
+    let bucket = client
+        .get_bucket()
+        .set_name(format!("projects/_/buckets/{bucket_id}"))
+        .send()
+        .await?;
+    let metageneration = bucket.metageneration;
+    let _bucket = client
+        .update_bucket()
+        .set_bucket(bucket.set_retention_policy(RetentionPolicy::new()))
+        .set_if_metageneration_match(metageneration)
+        .set_update_mask(FieldMask::default().set_paths(["retention_policy"]))
+        .send()
+        .await?;
+    println!("Retention policy for bucket {} removed.", bucket_id);
+    Ok(())
+}
+// [END storage_remove_retention_policy]
