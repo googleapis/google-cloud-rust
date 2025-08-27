f974aab2770748e56b553f3b1621d1d79776d857
Carlos O'Ryan <coryan@google.com>
2025-08-27 14:05:11 -0400

    Prompt:
    Use https://github.com/googleapis/google-cloud-cpp/blob/main/google/cloud/storage/examples/storage_default_object_acl_samples.cc
    translate the storage_add_bucket_default_owner and
    storage_remove_bucket_default_owner examples to Rust.

---
+++ src/storage/examples/src/buckets/remove_bucket_default_owner.rs
@@ -0,0 +1,44 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START storage_remove_bucket_default_owner]
+use google_cloud_storage::client::StorageControl;
+use google_cloud_wkt::FieldMask;
+
+pub async fn sample(
+    client: &StorageControl,
+    bucket_id: &str,
+    user_email: &str,
+) -> anyhow::Result<()> {
+    let bucket = client
+        .get_bucket()
+        .set_name(format!("projects/_/buckets/{bucket_id}"))
+        .send()
+        .await?;
+    let metageneration = bucket.metageneration;
+    let mut acls = bucket.default_object_acl.clone();
+    acls.retain(|acl| acl.entity != format!("user-{}", user_email));
+
+    let bucket = client
+        .update_bucket()
+        .set_bucket(bucket.set_default_object_acl(acls))
+        .set_if_metageneration_match(metageneration)
+        .set_update_mask(FieldMask::default().set_paths(["defaultObjectAcl"]))
+        .send()
+        .await?;
+    println!("Successfully removed default owner {user_email} from bucket {bucket_id}");
+    println!("{:?}", bucket.default_object_acl);
+    Ok(())
+}
+// [END storage_remove_bucket_default_owner]
