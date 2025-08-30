38c93324a8e1964024a51b8d49bf0b643877ad4e
Darren Bolduc <dbolduc@google.com>
2025-08-30 06:53:02 -0400

    docs(storage): copy file example (#3184)

--- src/storage/examples/src/lib.rs
+++ src/storage/examples/src/lib.rs
@@ -383,6 +383,7 @@ pub async fn run_object_examples(buckets: &mut Vec<String>) -> anyhow::Result<()
         "prefixes/are-not-always/folders-003",
         "prefixes/are-not-always/folders-004/abc",
         "prefixes/are-not-always/folders-004/def",
+        "object-to-copy",
         "object-to-update",
         "object-to-read",
         "deleted-object-name",
@@ -450,6 +451,8 @@ pub async fn run_object_examples(buckets: &mut Vec<String>) -> anyhow::Result<()
     objects::release_temporary_hold::sample(&control, &id).await?;
     tracing::info!("running delete_file example");
     objects::delete_file::sample(&control, &id).await?;
+    tracing::info!("running copy_file example");
+    objects::copy_file::sample(&control, &id, &id).await?;
     tracing::info!("running change_file_storage_class example");
     objects::change_file_storage_class::sample(&control, &id).await?;
     tracing::info!("running compose_file example");
