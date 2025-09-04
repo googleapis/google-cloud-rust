d73f7959ec88ada254d219393a0442606a0ddec5
Naveen Mandadhi <mandadhi@google.com>
2025-09-04 10:37:02 -0500

    docs(storage): add object print acl for user example (#3232)

--- src/storage/examples/src/lib.rs
+++ src/storage/examples/src/lib.rs
@@ -443,6 +443,8 @@ pub async fn run_object_examples(buckets: &mut Vec<String>) -> anyhow::Result<()
     objects::get_metadata::sample(&control, &id).await?;
     tracing::info!("running print_file_acl example");
     objects::print_file_acl::sample(&control, &id).await?;
+    tracing::info!("running print_file_acl_for_user example");
+    objects::print_file_acl_for_user::sample(&control, &id).await?;
     tracing::info!("running get_kms_key example");
     objects::get_kms_key::sample(&control, &id).await?;
     tracing::info!("running set_event_based_hold example");
