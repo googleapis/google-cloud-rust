cc6d0372ff5bcaedfd11542af1df76613643282a
Sai Sunder Srinivasan <saisunder@google.com>
2025-09-06 04:28:16 +0000

    doc(auth): Remove production usage warning on auth library

--- src/auth/src/lib.rs
+++ src/auth/src/lib.rs
@@ -14,11 +14,6 @@
 
 //! Google Cloud Client Libraries for Rust - Authentication Components
 //!
-//! **WARNING:** this crate is under active development. We expect multiple
-//! breaking changes in the upcoming releases. Testing is also incomplete, we do
-//! **not** recommend that you use this crate in production. We welcome feedback
-//! about the APIs, documentation, missing features, bugs, etc.
-//!
 //! This crate contains types and functions used to authenticate applications
 //! on Google Cloud.  The SDK clients consume an implementation of
 //! [credentials::Credentials] and use these credentials to authenticate RPCs
