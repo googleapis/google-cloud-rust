ad6f77f090df571bd7afc33fc8567ff92bc0bec7
Phong Chuong <phong.hoa.chuong@gmail.com>
2025-09-17 21:11:32 +0000

    allow clippy::collapsible_if

--- src/integration-tests/src/bigquery.rs
+++ src/integration-tests/src/bigquery.rs
@@ -253,6 +253,8 @@ pub async fn job_service(builder: bigquery::builder::job_service::ClientBuilder)
     Ok(())
 }
 
+// For nested if let chain.
+#[allow(clippy::collapsible_if)]
 async fn cleanup_stale_jobs(client: &bigquery::client::JobService, project_id: &str) -> Result<()> {
     use std::time::{Duration, SystemTime, UNIX_EPOCH};
     let stale_deadline = SystemTime::now().duration_since(UNIX_EPOCH)?;
@@ -293,19 +295,20 @@ async fn cleanup_stale_jobs(client: &bigquery::client::JobService, project_id: &
         .filter_map(|r| match r {
             Ok(r) => {
                 if let Some(job_reference) = &r.job_reference {
-                    if let Some(configuration) = &r.configuration
-                        && configuration
+                    if let Some(configuration) = &r.configuration {
+                        if configuration
                             .labels
                             .get(INSTANCE_LABEL)
                             .is_some_and(|v| v == "true")
-                    {
-                        return Some(
-                            client
-                                .delete_job()
-                                .set_project_id(project_id)
-                                .set_job_id(&job_reference.job_id)
-                                .send(),
-                        );
+                        {
+                            return Some(
+                                client
+                                    .delete_job()
+                                    .set_project_id(project_id)
+                                    .set_job_id(&job_reference.job_id)
+                                    .send(),
+                            );
+                        }
                     }
                 }
                 None
