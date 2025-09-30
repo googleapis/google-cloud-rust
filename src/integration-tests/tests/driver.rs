fecd1bc47841445a5e0a2a404d90e1c6641b43a7
Phong Chuong <phong.hoa.chuong@gmail.com>
2025-09-30 17:14:03 +0000

    update tracing code

--- src/integration-tests/tests/driver.rs
+++ src/integration-tests/tests/driver.rs
@@ -91,6 +91,7 @@ mod driver {
 
     #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
     async fn run_pubsub_basic_publisher() -> integration_tests::Result<()> {
+        let _guard = integration_tests::enable_tracing();
         integration_tests::pubsub::basic_publisher()
             .await
             .map_err(integration_tests::report_error)
