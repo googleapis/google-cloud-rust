6d8f01865fdf3da5a92a1760e9d751db03da0474
Phong Chuong <phong.hoa.chuong@gmail.com>
2025-10-02 18:50:39 +0000

    add missing values

--- .gcb/builds/triggers/main.tf
+++ .gcb/builds/triggers/main.tf
@@ -129,6 +129,8 @@ resource "google_cloudbuild_trigger" "pubsub-trigger" {
 
   source_to_build {
     repository = google_cloudbuildv2_repository.main.id
+    ref = "refs/heads/main"
+    repo_type = "GITHUB"
   }
 
   include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
