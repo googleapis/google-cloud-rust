2c8f10ad2c73ce7c670bff75babc73411daaf239
Alvaro Viebrantz <aviebrantz@google.com>
2025-10-07 21:15:16 +0000

    feat(auth): parse jwt id token as Token

--- src/auth/src/credentials/idtoken.rs
+++ src/auth/src/credentials/idtoken.rs
@@ -14,8 +14,14 @@
 
 use crate::Result;
 use crate::token::Token;
+use base64::{Engine as _, engine::general_purpose::URL_SAFE_NO_PAD};
+use gax::error::CredentialsError;
+use serde_json::Value;
+use std::collections::HashMap;
 use std::future::Future;
 use std::sync::Arc;
+use std::time::Duration;
+use tokio::time::Instant;
 
 /// Obtain [OIDC ID Tokens].
 ///
@@ -88,5 +94,82 @@ pub(crate) mod dynamic {
         async fn id_token(&self) -> Result<Token> {
             T::id_token(self).await
         }
+    }
+}
+
+/// parse JWT ID Token string as google_cloud_auth::token::Token
+pub(crate) fn parse_id_token_from_str(token: String) -> Result<Token> {
+    let parts: Vec<&str> = token.split('.').collect();
+    if parts.len() != 3 {
+        return Err(CredentialsError::from_msg(false, "invalid JWT token"));
+    }
+    let payload = URL_SAFE_NO_PAD
+        .decode(parts[1])
+        .map_err(|e| CredentialsError::from_source(false, e))?;
+
+    let claims: HashMap<String, Value> =
+        serde_json::from_slice(&payload).map_err(|e| CredentialsError::from_source(false, e))?;
+
+    let expires_at = claims["exp"]
+        .as_u64()
+        .map(|d| Instant::now() + Duration::from_secs(d)); // TODO: this is wrong, how to create an Instant from a seconds since epoch ?
+
+    let values = claims.into_iter().map(|(k, v)| {
+        let value = v.as_str().map(|v| v.to_string()).unwrap_or(v.to_string());
+        (k, value)
+    });
+
+    Ok(Token {
+        token,
+        token_type: "Bearer".to_string(),
+        expires_at,
+        metadata: Some(HashMap::from_iter(values)),
+    })
+}
+
+#[cfg(test)]
+pub(crate) mod tests {
+    use super::parse_id_token_from_str;
+    use super::*;
+    use base64::prelude::BASE64_URL_SAFE_NO_PAD;
+    use serial_test::parallel;
+    use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+    type TestResult = anyhow::Result<()>;
+
+    /// Function to be used in tests to generate a fake, but valid enough, id token.
+    pub(crate) fn generate_test_id_token<S: Into<String>>(audience: S) -> String {
+        let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+        let then = now + Duration::from_secs(3600);
+        let claims = serde_json::json!({
+            "iss": "test_iss".to_string(),
+            "aud": Some(audience.into()),
+            "exp": then.as_secs(),
+            "iat": now.as_secs(),
+        });
+
+        let json = serde_json::to_string(&claims).expect("failed to encode jwt claims");
+        let payload = BASE64_URL_SAFE_NO_PAD.encode(json.as_bytes());
+
+        format!("test_header.{}.test_signature", payload)
+    }
+
+    #[tokio::test]
+    #[parallel]
+    async fn test_parse_id_token() -> TestResult {
+        let audience = "https://example.com";
+        let id_token = generate_test_id_token(audience);
+
+        let token = parse_id_token_from_str(id_token.clone()).expect("should parse id token");
+
+        assert_eq!(token.token, id_token);
+        assert!(token.expires_at.is_some());
+
+        let metadata = token.metadata.expect("should parse claims as metadata");
+        assert_eq!(metadata["aud"], audience);
+
+        // TODO: check parsed expires_at
+
+        Ok(())
     }
 }
