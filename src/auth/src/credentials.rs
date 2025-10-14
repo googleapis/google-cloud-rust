2679f33428b461ef3c46833f177dad41d29a143b
Alvaro Viebrantz <aviebrantz@google.com>
2025-10-14 20:37:56 +0000

    feat: make idtoken public

--- src/auth/src/credentials.rs
+++ src/auth/src/credentials.rs
@@ -26,8 +26,7 @@ pub mod anonymous;
 pub mod api_key_credentials;
 pub mod external_account;
 pub(crate) mod external_account_sources;
-#[allow(dead_code)]
-pub(crate) mod idtoken;
+pub mod idtoken;
 pub mod impersonated;
 pub(crate) mod internal;
 pub mod mds;
