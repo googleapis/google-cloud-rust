e43628b244f97a6075597987bbf170cbf4002b6c
Carlos O'Ryan <coryan@google.com>
2025-11-21 19:13:25 -0500

    Pass more arguments to Bazel

--- .gcb/scripts/protojson-conformance.sh
+++ .gcb/scripts/protojson-conformance.sh
@@ -28,8 +28,24 @@ curl -fsSL --retry 5 --retry-delay 15 https://github.com/protocolbuffers/protobu
 curl -fsSL --retry 5 --retry-delay 15 -o /workspace/.bin/bazelisk https://github.com/bazelbuild/bazelisk/releases/download/v1.27.0/bazelisk-linux-amd64
 chmod 755 /workspace/.bin/bazelisk
 
+args=(
+    "--enable_bzlmod"
+    "--test_output=errors"
+    "--verbose_failures=true"
+    "--keep_going"
+    "--experimental_convenience_symlinks=ignore"
+    "--cache_test_results=$(should_cache_test_results)"
+  )
+if [[ -n "${BAZEL_REMOTE_CACHE:-}" ]]; then
+    args+=("--remote_cache=${BAZEL_REMOTE_CACHE}")
+    args+=("--google_default_credentials")
+    # See https://docs.bazel.build/versions/main/remote-caching.html#known-issues
+    # and https://github.com/bazelbuild/bazel/issues/3360
+    args+=("--experimental_guard_against_concurrent_changes")
+fi
+
 env -C /workspace/target/protobuf USE_BAZEL_VERSION=8.2.1 \
-    /workspace/.bin/bazelisk run --enable_bzlmod -- \
+    /workspace/.bin/bazelisk run "${args[@]}" -- \
     //conformance:conformance_test_runner \
     --failure_list /workspace/src/protojson-conformance/expected_failures.txt \
     /workspace/target/debug/protojson-conformance
