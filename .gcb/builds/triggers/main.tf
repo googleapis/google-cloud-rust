6a25171a105098075300ab32e24c319ccdb42ea9
Carlos O'Ryan <coryan@google.com>
2025-11-22 11:11:36 -0500

    ci: move `coverage` build to GCB

--- .gcb/builds/triggers/main.tf
+++ .gcb/builds/triggers/main.tf
@@ -47,6 +47,10 @@ locals {
       config = "complex.yaml"
       script = "compute-full"
     }
+    coverage = {
+      config = "coverage.yaml"
+      script = "coverage"
+    }
     docs = {
       config = "complex.yaml"
       script = "docs"
@@ -122,6 +126,10 @@ locals {
     compute-full = {
       config = "complex.yaml"
       script = "compute-full"
+    }
+    coverage = {
+      config = "coverage.yaml"
+      script = "coverage"
     }
     docs = {
       config = "complex.yaml"
---
+++ .gcb/scripts/coverage.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euv
+
+cargo version
+rustup component add rustfmt
+rustup show active-toolchain -v
+
+echo "==== Install cargo-tarpaulin ===="
+cargo install cargo-tarpaulin --version 0.32.1 --locked
+
+cargo tarpaulin --out xml
+
+args=(
+    # Get the codecov.io token from secret manager.
+    --token "${CODECOV_TOKEN:-}"
+    # We known where `cargo tarpaulin` outputs the coverage results, no need to
+    # search for them.
+    --file "cobertura.xml"
+    --disable-search
+    # Exit with an error if the upload fails, we can return the build in that
+    # case.
+    --fail-on-error
+    # Provide some basic information
+    --git-service github
+)
+# The commit SHA is required, use the GCB variable if set, otherwise try using
+# git to get the current SHA.
+if [[ -n "${COMMIT_SHA:-}" ]]; then
+    args+=(--pr "${_PR_NUMBER}")
+else
+    args+=(--pr "$(git rev-parse HEAD)")
+fi
+# Set the PR number from the GCB variable.
+if [[ -n "${_PR_NUMBER:-}" ]]; then
+    args+=(--pr "${_PR_NUMBER}")
+fi
+# Set the branch name from the GCB variable.
+if [[ -n "${BRANCH_NAME:-}" ]]; then
+    args+=(--branch "${BRANCH_NAME}")
+fi
+
+# Uploads the code coverate results
+env -i HOME="${HOME}" /workspace/.bin/codecovcli --verbose upload-process "${args[@]}"
