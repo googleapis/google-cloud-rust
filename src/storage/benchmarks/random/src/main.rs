5437e3994d62d88238e63d455e869958b318d0ab
Carlos O'Ryan <coryan@google.com>
2025-11-25 13:38:23 -0500

    Typos

--- src/storage/benchmarks/random/src/main.rs
+++ src/storage/benchmarks/random/src/main.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Benchmark random-reads using the Cloud Storage client library fo Rust.
+//! Benchmark random-reads using the Cloud Storage client library for Rust.
 
 mod args;
 mod dataset;
