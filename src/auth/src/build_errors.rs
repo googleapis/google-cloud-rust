1a2f6c6ebdfc2a4023411b4666929ed6e8d253ca
Alvaro Viebrantz <aviebrantz@google.com>
2025-12-12 15:19:04 -0400

    impl(auth): add ADC flow to obtain signing provider (#4056)
    Towards #3645

--- src/auth/src/build_errors.rs
+++ src/auth/src/build_errors.rs
@@ -48,7 +48,6 @@ impl Error {
         matches!(self.0, ErrorKind::MissingField(_))
     }
 
-    #[cfg(feature = "idtoken")]
     /// The credential type is not supported for the given use case.
     pub fn is_not_supported(&self) -> bool {
         matches!(self.0, ErrorKind::NotSupported(_))
@@ -84,7 +83,6 @@ impl Error {
         Error(ErrorKind::MissingField(field))
     }
 
-    #[cfg(feature = "idtoken")]
     /// The given credential type is not supported.
     pub(crate) fn not_supported<T>(credential_type: T) -> Error
     where
@@ -104,7 +102,6 @@ enum ErrorKind {
     UnknownType(#[source] BoxError),
     #[error("missing required field: {0}")]
     MissingField(&'static str),
-    #[cfg(feature = "idtoken")]
     #[error("credentials type not supported: {0}")]
     NotSupported(#[source] BoxError),
 }
