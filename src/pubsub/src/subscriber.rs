7a6edcddb560f081aa4f1bc1a2609cd03e7ffbec
Darren Bolduc <dbolduc@google.com>
2025-12-12 14:02:37 -0500

    impl(pubsub): subscriber at-least-once handler

--- src/pubsub/src/subscriber.rs
+++ src/pubsub/src/subscriber.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub(crate) mod handler;
 pub(crate) mod keepalive;
 pub(crate) mod lease_state;
 pub(crate) mod leaser;
