b74a40e7781e0307927a08a8129c1697816724d3
Carlos O'Ryan <coryan@google.com>
2026-01-09 10:22:01 -0500

    test(storage): non-default crypto providers
    Verify storage works with the default crypto provider and with
    non-default crypto providers. Later I will expand these tests to verify
    we can eliminate the dependency on the default crypto provider.

---
+++ tests/crypto-providers/storage-with-ring/src/main.rs
@@ -0,0 +1,27 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use rustls::crypto::{aws_lc_rs::default_provider, CryptoProvider};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // TODO(#4170) - verify no crypto provider is enabled. Currently
+    // `ring` is always installed, so we cannot enable the test.
+
+    // Install a default crypto provider and verify storage works.
+    CryptoProvider::install_default(default_provider())
+        .map_err(|p| anyhow::anyhow!("default provider was already installed: {p:?}"))?;
+
+    test_storage::run().await
+}
