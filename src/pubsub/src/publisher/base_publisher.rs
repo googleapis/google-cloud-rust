5686a0298c7ca111aafc1b8ea77e6ea3b5f9ea3d
Suzy Mueller <suzmue@google.com>
2026-01-29 00:37:29 +0000

    forgot cfg(test)

--- src/pubsub/src/publisher/base_publisher.rs
+++ src/pubsub/src/publisher/base_publisher.rs
@@ -14,7 +14,7 @@
 
 use crate::publisher::builder::PublisherPartialBuilder;
 
-/// Creates [`Publisher`](super::publisher::Publisher) instances.
+/// Creates [`Publisher`](super::client::Publisher) instances.
 ///
 /// A single `BasePublisher` can be used to create multiple `Publisher` clients
 /// for different topics. It manages the underlying gRPC connection and
