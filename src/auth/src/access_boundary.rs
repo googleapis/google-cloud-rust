895cff046fb63ed0d4c851d1a866f46cae4fc75d
Alvaro Viebrantz <aviebrantz@google.com>
2026-02-06 19:24:30 +0000

    fix: copyright year

--- src/auth/src/access_boundary.rs
+++ src/auth/src/access_boundary.rs
@@ -1,4 +1,4 @@
-// Copyright 2025 Google LLC
+// Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -143,7 +143,7 @@ where
 
     let mut state = RetryState::new(true);
 
-    loop {        
+    loop {
         match fetch_access_boundary(&token_provider, &url).await {
             Ok(val) => {
                 let _ = tx_header.send(val);
