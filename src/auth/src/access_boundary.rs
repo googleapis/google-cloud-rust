c92d0fc1b6977a05fff474cf9d1c3e39dc1fb8e9
Alvaro Viebrantz <aviebrantz@google.com>
2026-02-11 21:05:48 +0000

    impl(auth): handle expiration for trust boundary value

--- src/auth/src/access_boundary.rs
+++ src/auth/src/access_boundary.rs
@@ -22,21 +22,36 @@ use std::clone::Clone;
 use std::fmt::Debug;
 use std::sync::Arc;
 use tokio::sync::watch;
-use tokio::time::{Duration, sleep};
+use tokio::time::{Duration, Instant, sleep};
 
 const REGIONAL_ACCESS_BOUNDARIES_ENV_VAR: &str = "GOOGLE_AUTH_ENABLE_TRUST_BOUNDARIES";
 const NO_OP_ENCODED_LOCATIONS: &str = "0x0";
 
 // TTL: 6 hours
 const DEFAULT_TTL: Duration = Duration::from_secs(6 * 60 * 60);
-// Refresh interval: every hour
-const REFRESH_INTERVAL: Duration = Duration::from_secs(60 * 60);
+// Refresh slack: an hour before the TTL expires
+const REFRESH_SLACK: Duration = Duration::from_secs(60 * 60);
 // Period to wait after an error: 15 minutes
 const COOLDOWN_INTERVAL: Duration = Duration::from_secs(15 * 60);
 
 #[derive(Debug)]
 pub(crate) struct AccessBoundary {
-    rx_header: watch::Receiver<Option<String>>,
+    rx_header: watch::Receiver<Option<BoundaryValue>>,
+}
+
+#[derive(Debug, Clone)]
+struct BoundaryValue {
+    value: Option<String>,
+    expires_at: Instant,
+}
+
+impl BoundaryValue {
+    fn new(value: Option<String>) -> Self {
+        Self {
+            value,
+            expires_at: Instant::now() + DEFAULT_TTL,
+        }
+    }
 }
 
 impl AccessBoundary {
@@ -57,6 +72,17 @@ impl AccessBoundary {
         Self { rx_header }
     }
 
+    #[cfg(test)]
+    // only used for testing
+    pub(crate) fn new_with_mock_provider<T>(provider: T) -> Self
+    where
+        T: AccessBoundaryProvider + 'static,
+    {
+        let (tx_header, rx_header) = watch::channel(None);
+        tokio::spawn(refresh_task(Arc::new(provider), tx_header));
+        Self { rx_header }
+    }
+
     fn is_enabled() -> bool {
         std::env::var(REGIONAL_ACCESS_BOUNDARIES_ENV_VAR)
             .map(|v| v.to_lowercase())
@@ -65,20 +91,17 @@ impl AccessBoundary {
     }
 
     pub(crate) fn header_value(&self) -> Option<String> {
-        // TODO(#4186): handle expiration. Each new entry should have TTL per design doc.
         let val = self.rx_header.borrow().clone();
-        if val.as_ref().is_some_and(|v| v == NO_OP_ENCODED_LOCATIONS) {
-            return None;
-        }
-        val
+        val.filter(|b| b.expires_at >= Instant::now()) // fail open if expired
+            .and_then(|b| b.value)
+            .filter(|v| v != NO_OP_ENCODED_LOCATIONS)
     }
 }
 
 // internal trait for testability and avoid dependency on reqwest
 // which causes issues with tokio::time::advance and tokio::task::yield_now
-
 #[async_trait::async_trait]
-trait AccessBoundaryProvider: std::fmt::Debug + Send + Sync {
+pub(crate) trait AccessBoundaryProvider: std::fmt::Debug + Send + Sync {
     async fn fetch_access_boundary(&self) -> Result<Option<String>, CredentialsError>;
 }
 
@@ -155,15 +178,15 @@ where
     Ok(None)
 }
 
-async fn refresh_task<T>(provider: Arc<T>, tx_header: watch::Sender<Option<String>>)
+async fn refresh_task<T>(provider: Arc<T>, tx_header: watch::Sender<Option<BoundaryValue>>)
 where
     T: AccessBoundaryProvider,
 {
     loop {
         match provider.fetch_access_boundary().await {
             Ok(val) => {
-                let _ = tx_header.send(val);
-                sleep(REFRESH_INTERVAL).await;
+                let _ = tx_header.send(Some(BoundaryValue::new(val)));
+                sleep(DEFAULT_TTL - REFRESH_SLACK).await
             }
             Err(_e) => {
                 sleep(COOLDOWN_INTERVAL).await;
@@ -344,16 +367,18 @@ mod tests {
         assert!(val.is_none(), "{val:?}");
     }
 
-    #[test]
+    #[tokio::test]
     #[parallel]
-    fn test_access_boundary_header_value_no_op() {
+    async fn test_access_boundary_header_value_no_op() {
         let (tx, rx_header) = watch::channel(None);
         let access_boundary = AccessBoundary { rx_header };
 
-        let _ = tx.send(Some("0x123".to_string()));
+        let _ = tx.send(Some(BoundaryValue::new(Some("0x123".to_string()))));
         assert_eq!(access_boundary.header_value().as_deref(), Some("0x123"));
 
-        let _ = tx.send(Some(NO_OP_ENCODED_LOCATIONS.to_string()));
+        let _ = tx.send(Some(BoundaryValue::new(Some(
+            NO_OP_ENCODED_LOCATIONS.to_string(),
+        ))));
         let val = access_boundary.header_value();
         assert!(val.is_none(), "{val:?}");
 
@@ -403,7 +428,7 @@ mod tests {
             .times(1)
             .return_once(|| Ok(Some("0x123".to_string())));
 
-        let (tx, rx) = watch::channel::<Option<String>>(None);
+        let (tx, rx) = watch::channel::<Option<BoundaryValue>>(None);
 
         tokio::spawn(async move {
             refresh_task(Arc::new(mock_provider), tx).await;
@@ -431,6 +456,70 @@ mod tests {
         tokio::task::yield_now().await;
 
         let val = rx.borrow().clone();
-        assert_eq!(val.as_deref(), Some("0x123"));
+        let val = val.as_ref().and_then(|v| v.value.as_deref());
+        assert_eq!(val, Some("0x123"), "{val:?}");
+    }
+
+    #[tokio::test(start_paused = true)]
+    #[parallel]
+    async fn expired_access_boundary_returns_none() {
+        let (tx, rx_header) = watch::channel::<Option<BoundaryValue>>(None);
+        let access_boundary = AccessBoundary { rx_header };
+
+        let expires_at = Instant::now() + Duration::from_secs(10);
+        let _ = tx.send(Some(BoundaryValue {
+            value: Some("old-value".to_string()),
+            expires_at,
+        }));
+
+        // value is valid
+        let val = access_boundary.header_value();
+        assert_eq!(val.as_deref(), Some("old-value"), "{val:?}");
+
+        // advance time to expire the value
+        tokio::time::advance(Duration::from_secs(11)).await;
+
+        // value should return None if expired (non-blocking)
+        let val = access_boundary.header_value();
+        assert!(val.is_none(), "{val:?}");
+
+        // update with new value
+        let _ = tx.send(Some(BoundaryValue::new(Some("new-value".to_string()))));
+
+        let val = access_boundary.header_value();
+        assert_eq!(val.as_deref(), Some("new-value"), "{val:?}");
+    }
+
+    #[tokio::test(start_paused = true)]
+    #[parallel]
+    async fn access_boundary_provider_refreshes() {
+        let mut mock_provider = MockAccessBoundaryProvider::new();
+
+        mock_provider
+            .expect_fetch_access_boundary()
+            .times(1)
+            .returning(|| Ok(Some("old-value".to_string())));
+
+        mock_provider
+            .expect_fetch_access_boundary()
+            .times(1)
+            .returning(|| Ok(Some("new-value".to_string())));
+
+        let access_boundary = AccessBoundary::new_with_mock_provider(mock_provider);
+
+        // allow task to start and fail the first request
+        tokio::time::advance(Duration::from_secs(2)).await;
+        tokio::task::yield_now().await;
+
+        // value is valid
+        let val = access_boundary.header_value();
+        assert_eq!(val.as_deref(), Some("old-value"), "{val:?}");
+
+        // advance time beyond the time to refresh
+        tokio::time::advance(DEFAULT_TTL).await;
+        tokio::task::yield_now().await;
+
+        let val = access_boundary.header_value();
+        assert_eq!(val.as_deref(), Some("new-value"), "{val:?}");
     }
 }
