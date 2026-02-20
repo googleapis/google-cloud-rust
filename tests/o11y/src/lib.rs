3af2b1ff78fdcb58f73b8cbd2b9941a995661664
Carlos O'Ryan <coryan@google.com>
2026-02-19 19:00:05 -0500

    Remove tests for future features

--- tests/o11y/src/lib.rs
+++ tests/o11y/src/lib.rs
@@ -26,6 +26,4 @@ pub mod mock_collector;
 #[cfg(google_cloud_unstable_tracing)]
 pub mod otlp;
 #[cfg(google_cloud_unstable_tracing)]
-pub mod storage_tracing;
-#[cfg(google_cloud_unstable_tracing)]
 pub mod tracing;
