3cfc8bba949ba9825d0c9a7bee02f817f24d7b35
Sharon Lynn <sharonlynn@google.com>
2026-02-24 04:31:19 +0000

    docs(storage): Use head and tail for ranges and variable shadowing

--- src/storage/examples/src/objects/open_object_multiple_ranged_read.rs
+++ src/storage/examples/src/objects/open_object_multiple_ranged_read.rs
@@ -26,34 +26,30 @@ pub async fn sample(
 ) -> anyhow::Result<()> {
     let bucket_name = format!("projects/_/buckets/{bucket}");
 
-    // 1. Open the connection and request the first byte range. Read 1,024 bytes starting from offset 0.
-    let (descriptor, mut reader1) = client
+    // 1. Open the connection and request the first 5 bytes.
+    let (descriptor, mut reader) = client
         .open_object(bucket_name, object)
-        .send_and_read(ReadRange::segment(0, 1024))
+        .send_and_read(ReadRange::head(5))
         .await?;
 
-    // Create the file and stream the first discrete range directly to disk.
-    let file1_path = format!("{}_part1", destination_file_name);
-    let mut file1 = File::create(&file1_path).await?;
-    while let Some(data) = reader1.next().await.transpose()? {
-        file1.write_all(&data).await?;
+    let file_path = format!("{}_part1", destination_file_name);
+    let mut file = File::create(&file_path).await?;
+    while let Some(data) = reader.next().await.transpose()? {
+        file.write_all(&data).await?;
     }
-    file1.sync_all().await?;
-    println!("Downloaded bytes 0-1023 of object {object} in bucket {bucket} to {file1_path}.");
+    file.sync_all().await?;
+    println!("Downloaded the first 5 bytes of object {object} in bucket {bucket} to {file_path}.");
 
-    // 2. Request a second byte range using the existing `descriptor`.
-    // Read 2,048 bytes starting from offset 5000.
-    // Because the connection is already open, this read will have much lower latency.
-    let mut reader2 = descriptor.read_range(ReadRange::segment(5000, 2048)).await;
+    // 2. Request the last 2 bytes using the existing `descriptor`.
+    let mut reader = descriptor.read_range(ReadRange::tail(2)).await;
 
-    // Create the file and stream the second discrete range directly to disk.
-    let file2_path = format!("{}_part2", destination_file_name);
-    let mut file2 = File::create(&file2_path).await?;
-    while let Some(data) = reader2.next().await.transpose()? {
-        file2.write_all(&data).await?;
+    let file_path = format!("{}_part2", destination_file_name);
+    let mut file = File::create(&file_path).await?;
+    while let Some(data) = reader.next().await.transpose()? {
+        file.write_all(&data).await?;
     }
-    file2.sync_all().await?;
-    println!("Downloaded bytes 5000-7047 of object {object} in bucket {bucket} to {file2_path}.");
+    file.sync_all().await?;
+    println!("Downloaded the last 2 bytes of object {object} in bucket {bucket} to {file_path}.");
 
     println!("Metadata: {:?}", descriptor.object());
     Ok(())
