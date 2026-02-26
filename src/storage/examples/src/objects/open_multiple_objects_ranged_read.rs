2a0281aeed827a314a3a6f8bb0ea3db54bcc3be6
Sharon Lynn <sharonlynn@google.com>
2026-02-26 03:27:36 +0000

    Address review feedback

--- src/storage/examples/src/objects/open_multiple_objects_ranged_read.rs
+++ src/storage/examples/src/objects/open_multiple_objects_ranged_read.rs
@@ -15,33 +15,49 @@
 // [START storage_open_multiple_objects_ranged_read]
 use google_cloud_storage::client::Storage;
 use google_cloud_storage::model_ext::ReadRange;
+use google_cloud_storage::object_descriptor::ObjectDescriptor;
+use std::collections::HashMap;
 use tokio::task::JoinSet;
 
 pub async fn sample(client: &Storage, bucket: &str, object_names: &[&str]) -> anyhow::Result<()> {
-    let mut set = JoinSet::new();
     let bucket_name = format!("projects/_/buckets/{bucket}");
-    async fn  read_footer(client: &Storage, bucket: &str, object: &str)
-        -> anyhow::Result<(ObjectDescriptor, Vec<u8>)>
-    {
+
+    async fn read_footer(
+        client: &Storage,
+        bucket_name: &str,
+        object_name: &str,
+    ) -> anyhow::Result<(ObjectDescriptor, Vec<u8>, String)> {
         // Reading the last 8 bytes is common in Parquet and other column-oriented formats
         // with a footer.
-        let (descriptor, mut reader) = client.open_object(bucket_name.clone(), object)
+        let (descriptor, mut reader) = client
+            .open_object(bucket_name, object_name)
             .send_and_read(ReadRange::tail(8))
             .await?;
+
         let mut footer = Vec::new();
         while let Some(data) = reader.next().await.transpose()? {
-            content.extend_from_slice(&data);
+            footer.extend_from_slice(&data);
         }
-        Ok((descriptor, footer, object.to_string()))
-    };
-    
-    object_names.iter().for_each(|o| set.spawn(read_footer(client, &bucket_name, o.as_str())));
+        Ok((descriptor, footer, object_name.to_string()))
+    }
+
+    let mut set = JoinSet::new();
+    for o in object_names {
+        let client = client.clone();
+        let bucket_name = bucket_name.clone();
+        let object_name = o.to_string();
+        set.spawn(async move { read_footer(&client, &bucket_name, &object_name).await });
+    }
+
     let mut descriptors = HashMap::new();
     while let Some(result) = set.join_next().await {
-        let (descriptor, footer, name) = result??;
-        println!("The footer for {name} is {footer:?}");
-        descriptors.insert(name, (descriptor, footer));
+        let (descriptor, footer, object_name) = result??;
+        println!("The footer for object {object_name} is {footer:?}");
+        descriptors.insert(object_name, (descriptor, footer));
     }
+    
+    // You can use the descriptors in the map to read more ranges.
+
     Ok(())
 }
 // [END storage_open_multiple_objects_ranged_read]
