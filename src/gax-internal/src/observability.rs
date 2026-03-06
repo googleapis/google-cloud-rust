8fe5cafe7d471133b18cc299d4a349053f1cfe5e
Carlos O'Ryan <coryan@google.com>
2026-03-05 21:05:13 -0500

    impl(gax-internal): extends futures for client signals (#4908)

--- src/gax-internal/src/observability.rs
+++ src/gax-internal/src/observability.rs
@@ -39,7 +39,7 @@ pub mod grpc_tracing;
 mod client_signals;
 
 #[cfg(google_cloud_unstable_tracing)]
-pub use client_signals::{DurationMetric, RequestStart};
+pub use client_signals::{ClientSignalsExt, DurationMetric, RequestStart};
 
 #[cfg(google_cloud_unstable_tracing)]
 mod client_tracing;
