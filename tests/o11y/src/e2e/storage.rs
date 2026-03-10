979e0e0b2e47a1b358574fcbbd7f2afc738e7cc0
Carlos O'Ryan <coryan@google.com>
2026-03-10 11:07:32 -0400

    cleanup(integration-tests-o11y): simplify providers set up
    Running more than one integration test per process results in flaky
    tests, even if we initialize the providers only once. With this change,
    all the integration tests use the same function, and the function panics
    if another thread has called it first.

--- tests/o11y/src/e2e/storage.rs
+++ tests/o11y/src/e2e/storage.rs
@@ -12,13 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::e2e::wait_for_trace;
+use super::{new_credentials, set_up_providers, wait_for_trace};
 use google_cloud_storage::client::Storage;
 use google_cloud_storage::model_ext::ReadRange;
 use google_cloud_test_utils::runtime_config::project_id;
 use opentelemetry::trace::TraceContextExt;
 use std::collections::BTreeSet;
 use tracing_opentelemetry::OpenTelemetrySpanExt;
+use uuid::Uuid;
 
 const ROOT_SPAN_NAME: &str = "e2e-storage-test";
 
@@ -70,7 +71,15 @@ pub async fn run() -> anyhow::Result<()> {
 
 async fn send_trace(project_id: &str) -> anyhow::Result<String> {
     // 1. Setup Telemetry (Real Google Cloud Destination)
-    let provider = crate::e2e::set_up_tracer_provider(project_id).await?;
+    let id = Uuid::new_v4();
+    let credentials = new_credentials(project_id).await?;
+    let (provider, _meter_provider, _) = set_up_providers(
+        project_id,
+        ROOT_SPAN_NAME,
+        id.to_string(),
+        credentials.clone(),
+    )
+    .await?;
 
     // 2. Generate Trace
     // Start a root span
