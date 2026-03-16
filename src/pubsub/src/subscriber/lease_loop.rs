d13efcf94ce17591cad4aa23c82221c47c258720
Phong Chuong <phong.hoa.chuong@gmail.com>
2026-03-16 16:33:39 +0000

    fix validatin

--- src/pubsub/src/subscriber/lease_loop.rs
+++ src/pubsub/src/subscriber/lease_loop.rs
@@ -208,7 +208,7 @@ mod tests {
             .await
             .expect_confirmed_ack()
             .times(1)
-            .withf(|v| v == test_ids(0..1))
+            .withf(|v| v.len() == 1 && v[0] == test_id(0))
             .returning(|_| ());
         tokio::time::advance(FLUSH_START).await;
 
