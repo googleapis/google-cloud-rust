33762941825bff5bcc7f1271c9d9432d0d5ace04
Carlos O'Ryan <coryan@google.com>
2026-03-17 18:18:53 -0400

    cleanup: no `lazy_static` in hand-crafted code
    I am not sure why we started using `lazy_static`. Maybe we did not know
    about `LazyLock`, maybe we started with a MSRV below 1.80.  In any case,
    we do not need this dependency.
    
    This removes uses from hand-crafted code. We still need to remove them
    from the generated code before we can expunge the dependency.

--- src/pubsub/src/lib.rs
+++ src/pubsub/src/lib.rs
@@ -181,18 +181,18 @@ pub mod stub {
 const DEFAULT_HOST: &str = "https://pubsub.googleapis.com";
 
 mod info {
+    use std::sync::LazyLock;
+
     const NAME: &str = env!("CARGO_PKG_NAME");
     const VERSION: &str = env!("CARGO_PKG_VERSION");
-    lazy_static::lazy_static! {
-        pub(crate) static ref X_GOOG_API_CLIENT_HEADER: String = {
-            let ac = gaxi::api_header::XGoogApiClient{
-                name:          NAME,
-                version:       VERSION,
-                library_type:  gaxi::api_header::GAPIC,
-            };
-            ac.grpc_header_value()
+    pub(crate) static X_GOOG_API_CLIENT_HEADER: LazyLock<String> = LazyLock::new(|| {
+        let ac = gaxi::api_header::XGoogApiClient {
+            name: NAME,
+            version: VERSION,
+            library_type: gaxi::api_header::GAPIC,
         };
-    }
+        ac.grpc_header_value()
+    });
 }
 
 #[allow(dead_code)]
