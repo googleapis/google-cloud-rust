976464acbefed3abfba2a7f536fc79bdd8922429
Knut Olav Løite <koloite@gmail.com>
2026-03-25 17:51:15 +0100

    test(spanner): integration tests for read/write transactions (#5135)
    Adds integration tests for read/write transactions. Verifies that the
    transaction runner automatically commits the transaction if the
    transaction is successful, and automatically rollbacks the transaction
    in case of an error.

--- tests/spanner/tests/driver.rs
+++ tests/spanner/tests/driver.rs
@@ -44,6 +44,25 @@ mod spanner {
     }
 
     #[tokio::test]
+    async fn run_read_write_tests() -> anyhow::Result<()> {
+        let db_client = match integration_tests_spanner::client::create_database_client().await {
+            Some(c) => c,
+            None => return Ok(()),
+        };
+
+        integration_tests_spanner::read_write_transaction::successful_read_write_transaction(
+            &db_client,
+        )
+        .await?;
+        integration_tests_spanner::read_write_transaction::rolled_back_read_write_transaction(
+            &db_client,
+        )
+        .await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
     async fn run_partitioned_dml_tests() -> anyhow::Result<()> {
         let db_client = match integration_tests_spanner::client::create_database_client().await {
             Some(c) => c,
