b1ae79f5310dd222298d9443959537c8256073fa
Knut Olav Løite <koloite@gmail.com>
2026-03-30 18:28:13 +0200

    feat(spanner): execute a batch read-only transaction partition
    Adds a method for executing a Partition that has previously been returned by a
    partition_read or partition_query operation. A Partition can be executed using
    any DatabaseClient that is connected to the same database.

--- tests/spanner/src/lib.rs
+++ tests/spanner/src/lib.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub mod batch_read_only_transaction;
 pub mod client;
 pub mod partitioned_dml;
 pub mod query;
