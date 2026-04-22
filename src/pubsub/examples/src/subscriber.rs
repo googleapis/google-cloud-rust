562640dd4d223edd11fd5839a7c9e0eedf146cdd
Darren Bolduc <dbolduc@google.com>
2026-04-22 11:17:49 -0400

    docs(pubsub): add subscriber concurrency control sample (#5483)
    Fixes #5437

--- src/pubsub/examples/src/subscriber.rs
+++ src/pubsub/examples/src/subscriber.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub mod concurrency_control;
 pub mod exactly_once;
 pub mod flow_settings;
 pub mod quickstart_subscriber;
