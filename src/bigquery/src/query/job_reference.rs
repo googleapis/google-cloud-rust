492fbee4d0a189258c6630bc87976a9e24e2acdf
Alvaro Viebrantz <aviebrantz@google.com>
2026-05-15 17:20:25 +0000

    test: add basic test for job ref

--- src/bigquery/src/query/job_reference.rs
+++ src/bigquery/src/query/job_reference.rs
@@ -15,7 +15,7 @@
 #![allow(dead_code, unused_imports)]
 
 /// Job Reference.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub(crate) enum JobReference {
     StatefulJob {
         job_id: String,
@@ -56,5 +56,56 @@ impl JobReference {
             ),
             Self::StatelessJob { .. } => None,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn stateful_job() {
+        // With location
+        let proto = google_cloud_bigquery_v2::model::JobReference::new()
+            .set_project_id("a-project-id")
+            .set_job_id("a-job-id")
+            .set_location("US");
+        let job_ref = JobReference::from(proto.clone());
+        assert_eq!(
+            job_ref,
+            JobReference::StatefulJob {
+                job_id: "a-job-id".to_string(),
+                project_id: "a-project-id".to_string(),
+                location: Some("US".to_string()),
+            }
+        );
+        assert_eq!(job_ref.as_job_ref(), Some(proto));
+
+        // Without location
+        let proto = google_cloud_bigquery_v2::model::JobReference::new()
+            .set_project_id("a-project-id")
+            .set_job_id("a-job-id");
+        let job_ref = JobReference::from(proto.clone());
+        assert_eq!(
+            job_ref,
+            JobReference::StatefulJob {
+                job_id: "a-job-id".to_string(),
+                project_id: "a-project-id".to_string(),
+                location: None,
+            }
+        );
+        assert_eq!(job_ref.as_job_ref(), Some(proto));
+    }
+
+    #[test]
+    fn stateless_job() {
+        let job_ref = JobReference::from_query_id("a-query-id".to_string());
+        assert_eq!(
+            job_ref,
+            JobReference::StatelessJob {
+                query_id: "a-query-id".to_string(),
+            }
+        );
+        assert_eq!(job_ref.as_job_ref(), None);
     }
 }
