265f1b4b3e7adf7e2e82c5fd0bde4f3cbd7d2674
Knut Olav Løite <koloite@gmail.com>
2026-05-27 15:26:56 +0200

    docs(spanner): add samples for creating a client and a database (#5739)
    Adds samples for creating a client and a database for Spanner.
    
    Also adds a convinience method for getting a DatabaseAdminClient
    directly from the Spanner client. This convinience method also respects
    the SPANNER_EMULATOR_HOST environment variable.
    
    Updates #4973

--- src/spanner/examples/src/database/mod.rs
+++ src/spanner/examples/src/database/mod.rs
@@ -12,6 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod client;
-pub mod database;
-pub mod query;
+pub mod create_database;
+pub mod pg_create_database;
