807fac401f8bf634d82e7bd1472f9b9c3ffc5936
haphungw <haphung@google.com>
2026-06-10 05:22:23 +0000

    feat(bigquery): restrict GAPIC poller to InsertJob and remove redundant params
    - Removed the generic `PollerExt` trait and replaced it with `InsertJobBuilderExt`.
    - Extracted `project_id`, `location`, and `stub` internally using the builder's raw `pub(crate)` fields, achieving a 0-argument `poller()` method.
    - The GAPIC `poller` is now tightly scoped to `InsertJob`, leaving other job types (like `jobs.query`) to be handled by the handwritten Query client.

--- src/generated/cloud/bigquery/v2/src/operation.rs
+++ src/generated/cloud/bigquery/v2/src/operation.rs
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::builder::job_service::{GetQueryResults, InsertJob};
-use crate::client::JobService;
-use crate::model::{GetQueryResultsResponse, Job};
+use crate::builder::job_service::InsertJob;
+
+use crate::model::Job;
 use google_cloud_lro::Poller;
 use google_cloud_lro::internal::DiscoveryOperation;
 
@@ -25,16 +25,6 @@ impl DiscoveryOperation for Job {
             .map(|s| s.state.as_str())
             .unwrap_or_default()
             == "DONE"
-    }
-
-    fn name(&self) -> Option<&String> {
-        self.job_reference.as_ref().map(|r| &r.job_id)
-    }
-}
-
-impl DiscoveryOperation for GetQueryResultsResponse {
-    fn done(&self) -> bool {
-        self.job_complete == Some(true)
     }
 
     fn name(&self) -> Option<&String> {
@@ -62,7 +52,7 @@ impl DiscoveryOperation for GetQueryResultsResponse {
 ///                 )
 ///             )
 ///         )
-///         .poller(&client, project_id, None);
+///         .poller();
 ///     
 ///     // Wait for the job to complete
 ///     let job = poller.until_done().await?;
@@ -78,23 +68,20 @@ impl DiscoveryOperation for GetQueryResultsResponse {
 /// ```
 pub trait InsertJobBuilderExt {
     /// Returns a poller to monitor the status of the inserted job.
-    fn poller(
-        self,
-        client: &JobService,
-        project_id: impl Into<String>,
-        location: Option<String>,
-    ) -> impl Poller<Job, Job>;
+    fn poller(self) -> impl Poller<Job, Job>;
 }
 
 impl InsertJobBuilderExt for InsertJob {
-    fn poller(
-        self,
-        client: &JobService,
-        project_id: impl Into<String>,
-        location: Option<String>,
-    ) -> impl Poller<Job, Job> {
-        let client_clone = client.clone();
-        let project_id = project_id.into();
+    fn poller(self) -> impl Poller<Job, Job> {
+        let req = &self.0.request;
+        let project_id = req.project_id.clone();
+        let location = req
+            .job
+            .as_ref()
+            .and_then(|j| j.job_reference.as_ref())
+            .and_then(|r| r.location.clone());
+
+        let stub = self.0.stub.clone();
 
         let start = move || {
             let req = self;
@@ -102,90 +89,11 @@ impl InsertJobBuilderExt for InsertJob {
         };
 
         let query = move |name: String| {
-            let client = client_clone.clone();
+            let stub = stub.clone();
             let project_id = project_id.clone();
             let location = location.clone();
             async move {
-                let mut b = client.get_job().set_project_id(project_id).set_job_id(name);
-                if let Some(loc) = location {
-                    b = b.set_location(loc);
-                }
-                let mut options = google_cloud_gax::options::RequestOptions::default();
-                options.set_retry_policy(google_cloud_gax::retry_policy::NeverRetry);
-                b.with_options(options).send().await
-            }
-        };
-
-        let polling_error_policy =
-            std::sync::Arc::new(google_cloud_gax::polling_error_policy::Aip194Strict);
-        let polling_backoff_policy = std::sync::Arc::new(
-            google_cloud_gax::exponential_backoff::ExponentialBackoff::default(),
-        );
-
-        google_cloud_lro::internal::new_discovery_poller(
-            polling_error_policy,
-            polling_backoff_policy,
-            start,
-            query,
-        )
-    }
-}
-
-/// Extension trait for [`GetQueryResults`] to support Long-Running Operation (LRO) polling.
-///
-/// # Example
-/// ```no_run
-/// use google_cloud_bigquery_v2::client::JobService;
-/// use google_cloud_bigquery_v2::operation::GetQueryResultsBuilderExt;
-/// use google_cloud_lro::Poller;
-///
-/// async fn example(client: JobService, project_id: &str, job_id: &str) -> Result<(), google_cloud_gax::error::Error> {
-///     let mut poller = client
-///         .get_query_results()
-///         .set_project_id(project_id)
-///         .set_job_id(job_id)
-///         .poller(&client, project_id, None);
-///     
-///     let response = poller.until_done().await?;
-///     
-///     if response.job_complete.unwrap_or(false) {
-///         println!("Query finished! Received {} rows", response.total_rows.unwrap_or_default());
-///     }
-///     Ok(())
-/// }
-/// ```
-pub trait GetQueryResultsBuilderExt {
-    /// Returns a poller to monitor the status of the query results.
-    fn poller(
-        self,
-        client: &JobService,
-        project_id: impl Into<String>,
-        location: Option<String>,
-    ) -> impl Poller<GetQueryResultsResponse, GetQueryResultsResponse>;
-}
-
-impl GetQueryResultsBuilderExt for GetQueryResults {
-    fn poller(
-        self,
-        client: &JobService,
-        project_id: impl Into<String>,
-        location: Option<String>,
-    ) -> impl Poller<GetQueryResultsResponse, GetQueryResultsResponse> {
-        let client_clone = client.clone();
-        let project_id = project_id.into();
-
-        let start = move || {
-            let req = self;
-            async move { req.send().await }
-        };
-
-        let query = move |name: String| {
-            let client = client_clone.clone();
-            let project_id = project_id.clone();
-            let location = location.clone();
-            async move {
-                let mut b = client
-                    .get_query_results()
+                let mut b = crate::builder::job_service::GetJob::new(stub)
                     .set_project_id(project_id)
                     .set_job_id(name);
                 if let Some(loc) = location {
