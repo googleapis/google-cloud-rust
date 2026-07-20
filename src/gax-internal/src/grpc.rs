a471e686b2bd75bed9746e19e037fcdb23323c5c
Joshua Tan <joshuatan@google.com>
2026-07-20 03:42:30 +0000

    feat(gax): add grpc-rust response pump

--- src/gax-internal/src/grpc.rs
+++ src/gax-internal/src/grpc.rs
@@ -17,7 +17,7 @@
 pub mod from_status;
 mod grpc_helpers;
 #[cfg(google_cloud_unstable_grpc_rust)]
-mod grpc_rust;
+pub mod grpc_rust;
 pub mod status;
 #[cfg(google_cloud_unstable_gapic_streaming)]
 pub(crate) mod streaming;
