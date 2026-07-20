bbd23accc95d4b945848cbca41bf8a7f64fa6f67
Alvaro Viebrantz <aviebrantz@google.com>
2026-07-20 18:03:03 +0000

    docs(bigquery): add table partitioning and destination table samples

---
+++ src/bigquery/examples/src/query/partitioned_table.rs
@@ -0,0 +1,42 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START bigquery_query_partitioned_table]
+use google_cloud_bigquery::client::BigQuery;
+
+pub async fn sample(project_id: &str) -> anyhow::Result<()> {
+    let client = BigQuery::builder().build().await?;
+
+    let mut rows = client
+        .query(
+            "SELECT \
+        block_number FROM `bigquery-public-data.crypto_bitcoin.transactions` \
+        WHERE DATE(block_timestamp) = '2018-01-01' \
+        LIMIT 10",
+        )
+        .with_project_id(project_id)
+        .set_location("US")
+        .run()
+        .await?
+        .until_done()
+        .await?
+        .read();
+
+    while let Some(row) = rows.next().await.transpose()? {
+        let block_num: i64 = row.get("block_number");
+        println!("Block number: {block_num}");
+    }
+    Ok(())
+}
+// [END bigquery_query_partitioned_table]
