524c3eda74fac1fc3f67307aed1aed947f6cccb5
Darren Bolduc <dbolduc@google.com>
2026-07-23 12:16:08 -0400

    impl(bigquery): add write stream event loop (#6143)
    Towards #5831

--- src/bigquery-write/src/runner.rs
+++ src/bigquery-write/src/runner.rs
@@ -17,10 +17,14 @@ use crate::error::{AppendError, AppendResult};
 use crate::google::cloud::bigquery::storage::v1::{AppendRowsRequest, AppendRowsResponse};
 use crate::stream::Stream;
 use crate::transport::Transport;
+use gaxi::grpc::from_status::to_gax_error;
+use gaxi::grpc::tonic::{Status as TonicStatus, Streaming};
 use std::collections::VecDeque;
 use std::sync::Arc;
 use tokio::sync::{mpsc, oneshot};
 use tokio::task::JoinHandle;
+
+type TonicResult<T> = std::result::Result<T, TonicStatus>;
 
 #[derive(Debug)]
 pub(crate) struct WriteRequest {
@@ -69,15 +73,56 @@ async fn run_stream_task(inner: Arc<Transport>, mut req_rx: mpsc::Receiver<Write
     resp_txs.push_back(initial_req.resp_tx);
 
     // Open the stream.
-    let Stream { stream, request_tx } = match Stream::new(inner, initial_req.req).await {
+    let Stream {
+        mut stream,
+        request_tx,
+    } = match Stream::new(inner, initial_req.req).await {
         Ok(s) => s,
         Err(e) => {
             process_gax_response(&mut resp_txs, Err(e));
             return;
         }
     };
-    // TODO(#5831) - implement stream event loop
-    let _ = (stream, request_tx);
+
+    loop {
+        tokio::select! {
+            req = req_rx.recv() => {
+                match req {
+                    Some(r) => {
+                        // Keep track of the response channel.
+                        resp_txs.push_back(r.resp_tx);
+
+                        // Forward the request to the stream.
+                        let _ = request_tx.send(r.req).await;
+                    }
+                    None => break drain_stream(stream, resp_txs).await,
+                }
+            }
+            resp = stream.message() => {
+                match resp.transpose() {
+                    Some(r) => process_response(&mut resp_txs, r),
+                    // Note that tonic yields `None` after an `Err(e)`.
+                    None => break,
+                }
+            }
+        }
+    }
+}
+
+async fn drain_stream(
+    mut stream: Streaming<AppendRowsResponse>,
+    mut resp_txs: VecDeque<oneshot::Sender<AppendResult<AppendRowsResponse>>>,
+) {
+    while let Some(r) = stream.message().await.transpose() {
+        process_response(&mut resp_txs, r);
+    }
+}
+
+fn process_response(
+    resp_txs: &mut VecDeque<oneshot::Sender<AppendResult<AppendRowsResponse>>>,
+    resp: TonicResult<AppendRowsResponse>,
+) {
+    process_gax_response(resp_txs, resp.map_err(to_gax_error))
 }
 
 fn process_gax_response(
@@ -96,9 +141,12 @@ fn process_gax_response(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::google::cloud::bigquery::storage::v1::append_rows_response::{
+        AppendResult, Response,
+    };
     use crate::transport::tests::*;
     use bigquery_write_grpc_mock::{MockBigQueryWrite, start};
-    use gaxi::grpc::tonic::{Response as TonicResponse, Status as TonicStatus};
+    use gaxi::grpc::tonic::Response as TonicResponse;
     use google_cloud_gax::error::rpc::Code;
 
     #[tokio::test]
@@ -138,10 +186,39 @@ mod tests {
         };
         req_tx.send(write1).await?;
 
-        // TODO(#5831) - write to and read from the stream.
-        drop(response_tx);
+        // write 2
+        let (resp_tx2, resp_rx2) = oneshot::channel();
+        let write2 = WriteRequest {
+            req: test_request(2),
+            resp_tx: resp_tx2,
+        };
+        req_tx.send(write2).await?;
+
+        // resp 1
+        response_tx.send(Ok(convert(&test_response(1)))).await?;
+        let resp1 = resp_rx1.await??;
+        assert_eq!(resp1, test_response(1));
+
+        // write 3
+        let (resp_tx3, resp_rx3) = oneshot::channel();
+        let write3 = WriteRequest {
+            req: test_request(3),
+            resp_tx: resp_tx3,
+        };
+        req_tx.send(write3).await?;
+
+        // resp 2
+        response_tx.send(Ok(convert(&test_response(2)))).await?;
+        let resp2 = resp_rx2.await??;
+        assert_eq!(resp2, test_response(2));
+
+        // resp 3
+        response_tx.send(Ok(convert(&test_response(3)))).await?;
+        let resp3 = resp_rx3.await??;
+        assert_eq!(resp3, test_response(3));
+
         drop(req_tx);
-        let _ = resp_rx1.await;
+        drop(response_tx);
         handle.await?;
 
         Ok(())
@@ -180,10 +257,198 @@ mod tests {
         Ok(())
     }
 
+    #[tokio::test]
+    async fn error_mid_stream() -> anyhow::Result<()> {
+        let (response_tx, response_rx) = mpsc::channel(10);
+        let mut mock = MockBigQueryWrite::new();
+        mock.expect_append_rows()
+            .return_once(|_| Ok(TonicResponse::from(response_rx)));
+        let (endpoint, _server) = start("0.0.0.0:0", mock).await?;
+        let transport = Arc::new(test_transport(endpoint).await?);
+
+        let Runner { req_tx, handle } = Runner::new(transport);
+
+        // write 1
+        let (resp_tx1, resp_rx1) = oneshot::channel();
+        let write1 = WriteRequest {
+            req: test_request(1),
+            resp_tx: resp_tx1,
+        };
+        req_tx.send(write1).await?;
+
+        // write 2
+        let (resp_tx2, resp_rx2) = oneshot::channel();
+        let write2 = WriteRequest {
+            req: test_request(2),
+            resp_tx: resp_tx2,
+        };
+        req_tx.send(write2).await?;
+
+        // write 3
+        let (resp_tx3, resp_rx3) = oneshot::channel();
+        let write3 = WriteRequest {
+            req: test_request(3),
+            resp_tx: resp_tx3,
+        };
+        req_tx.send(write3).await?;
+
+        // resp 1
+        response_tx.send(Ok(convert(&test_response(1)))).await?;
+        let resp1 = resp_rx1.await??;
+        assert_eq!(resp1, test_response(1));
+
+        // resp 2 - error
+        response_tx
+            .send(Err(TonicStatus::failed_precondition("fail")))
+            .await?;
+        let resp2 = resp_rx2.await?;
+        let Err(AppendError::Rpc { source: err }) = resp2 else {
+            anyhow::bail!("expected an RPC error, got: {resp2:?}");
+        };
+        let Some(status) = err.status() else {
+            anyhow::bail!("expected a status, got: {err:?}");
+        };
+        assert_eq!(status.code, Code::FailedPrecondition);
+        assert_eq!(status.message, "fail");
+
+        // resp 3 - channel closed error
+        let _resp3 = resp_rx3.await.expect_err("channel should be closed");
+
+        drop(req_tx);
+        drop(response_tx);
+        handle.await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn sender_dropped_mid_stream() -> anyhow::Result<()> {
+        let (response_tx, response_rx) = mpsc::channel(10);
+        let mut mock = MockBigQueryWrite::new();
+        mock.expect_append_rows()
+            .return_once(|_| Ok(TonicResponse::from(response_rx)));
+        let (endpoint, _server) = start("0.0.0.0:0", mock).await?;
+        let transport = Arc::new(test_transport(endpoint).await?);
+
+        let Runner { req_tx, handle } = Runner::new(transport);
+
+        // write 1
+        let (resp_tx1, resp_rx1) = oneshot::channel();
+        let write1 = WriteRequest {
+            req: test_request(1),
+            resp_tx: resp_tx1,
+        };
+        req_tx.send(write1).await?;
+
+        // write 2
+        let (resp_tx2, resp_rx2) = oneshot::channel();
+        let write2 = WriteRequest {
+            req: test_request(2),
+            resp_tx: resp_tx2,
+        };
+        req_tx.send(write2).await?;
+
+        // write 3
+        let (resp_tx3, resp_rx3) = oneshot::channel();
+        let write3 = WriteRequest {
+            req: test_request(3),
+            resp_tx: resp_tx3,
+        };
+        req_tx.send(write3).await?;
+
+        // resp 1
+        response_tx.send(Ok(convert(&test_response(1)))).await?;
+        let resp1 = resp_rx1.await??;
+        assert_eq!(resp1, test_response(1));
+
+        // Drop the request sender before all the writes are finished.
+        drop(req_tx);
+
+        // resp 2
+        response_tx.send(Ok(convert(&test_response(2)))).await?;
+        let resp2 = resp_rx2.await??;
+        assert_eq!(resp2, test_response(2));
+
+        // resp 3
+        response_tx.send(Ok(convert(&test_response(3)))).await?;
+        let resp3 = resp_rx3.await??;
+        assert_eq!(resp3, test_response(3));
+
+        drop(response_tx);
+        handle.await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn unexpected_end_of_stream() -> anyhow::Result<()> {
+        // If the stream ends without responding to us, the service broke its contract. It is easy
+        // enough to be defensive.
+        let (response_tx, response_rx) = mpsc::channel(10);
+        let mut mock = MockBigQueryWrite::new();
+        mock.expect_append_rows()
+            .return_once(|_| Ok(TonicResponse::from(response_rx)));
+        let (endpoint, _server) = start("0.0.0.0:0", mock).await?;
+        let transport = Arc::new(test_transport(endpoint).await?);
+
+        let Runner { req_tx, handle } = Runner::new(transport);
+
+        // write 1
+        let (resp_tx1, resp_rx1) = oneshot::channel();
+        let write1 = WriteRequest {
+            req: test_request(1),
+            resp_tx: resp_tx1,
+        };
+        req_tx.send(write1).await?;
+
+        // write 2
+        let (resp_tx2, resp_rx2) = oneshot::channel();
+        let write2 = WriteRequest {
+            req: test_request(2),
+            resp_tx: resp_tx2,
+        };
+        req_tx.send(write2).await?;
+
+        // write 3
+        let (resp_tx3, resp_rx3) = oneshot::channel();
+        let write3 = WriteRequest {
+            req: test_request(3),
+            resp_tx: resp_tx3,
+        };
+        req_tx.send(write3).await?;
+
+        // resp 1
+        response_tx.send(Ok(convert(&test_response(1)))).await?;
+        let resp1 = resp_rx1.await??;
+        assert_eq!(resp1, test_response(1));
+
+        // Close the stream
+        drop(response_tx);
+
+        // resp 2 - channel closed error
+        let _resp2 = resp_rx2.await.expect_err("channel should be closed");
+
+        // resp 3 - channel closed error
+        let _resp3 = resp_rx3.await.expect_err("channel should be closed");
+        handle.await?;
+
+        Ok(())
+    }
+
     fn test_request(index: i64) -> AppendRowsRequest {
         AppendRowsRequest {
             write_stream: "projects/p/datasets/d/tables/t/streams/s".to_string(),
             offset: Some(index),
+            ..Default::default()
+        }
+    }
+
+    fn test_response(index: i64) -> AppendRowsResponse {
+        AppendRowsResponse {
+            response: Some(Response::AppendResult(AppendResult {
+                offset: Some(index + 1),
+            })),
+            write_stream: "projects/p/datasets/d/tables/t/streams/s".to_string(),
             ..Default::default()
         }
     }
