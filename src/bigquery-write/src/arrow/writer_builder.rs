a3ca1ccd584c46f27b3210cc6ad922ac71d1f4f9
Darren Bolduc <dbolduc@google.com>
2026-07-31 20:59:01 +0000

    impl(bigquery): add arrow API surface

--- src/bigquery-write/src/arrow/writer_builder.rs
+++ src/bigquery-write/src/arrow/writer_builder.rs
@@ -26,7 +26,6 @@ pub struct WriterBuilder {
 }
 
 impl WriterBuilder {
-    #[cfg_attr(not(test), expect(dead_code))]
     pub(crate) fn new(inner: Arc<Transport>, schema: ArrowSchema) -> Self {
         Self { inner, schema }
     }
