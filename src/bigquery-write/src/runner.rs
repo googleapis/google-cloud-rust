2ab76373fb46b784b238ce1742ec70db9e8b138d
Darren Bolduc <dbolduc@google.com>
2026-07-31 21:11:10 +0000

    yes, bot

--- src/bigquery-write/src/runner.rs
+++ src/bigquery-write/src/runner.rs
@@ -48,17 +48,18 @@ pub(crate) struct WriteRequest {
 #[derive(Debug)]
 pub(crate) struct Runner {
     pub(crate) req_tx: mpsc::Sender<WriteRequest>,
-    pub(crate) _handle: JoinHandle<()>,
+    #[allow(dead_code)]
+    pub(crate) handle: JoinHandle<()>,
 }
 
 impl Runner {
     pub(crate) fn new(inner: Arc<Transport>) -> Self {
         // TODO(#6122) - configure flow control settings
         let (req_tx, req_rx) = mpsc::channel(100);
-        let _handle = tokio::spawn(async move {
+        let handle = tokio::spawn(async move {
             run_stream_task(inner, req_rx).await;
         });
-        Runner { req_tx, _handle }
+        Runner { req_tx, handle }
     }
 }
 
@@ -159,11 +160,11 @@ pub(crate) mod tests {
         let (endpoint, _server) = start("0.0.0.0:0", mock).await?;
         let transport = Arc::new(test_transport(endpoint).await?);
 
-        let Runner { req_tx, _handle } = Runner::new(transport);
+        let Runner { req_tx, handle } = Runner::new(transport);
 
         // Drop the request sender before making any requests.
         drop(req_tx);
-        _handle.await?;
+        handle.await?;
 
         Ok(())
     }
@@ -177,7 +178,7 @@ pub(crate) mod tests {
         let (endpoint, _server) = start("0.0.0.0:0", mock).await?;
         let transport = Arc::new(test_transport(endpoint).await?);
 
-        let Runner { req_tx, _handle } = Runner::new(transport);
+        let Runner { req_tx, handle } = Runner::new(transport);
 
         // write 1
         let (resp_tx1, resp_rx1) = oneshot::channel();
@@ -220,7 +221,7 @@ pub(crate) mod tests {
 
         drop(req_tx);
         drop(response_tx);
-        _handle.await?;
+        handle.await?;
 
         Ok(())
     }
@@ -233,7 +234,7 @@ pub(crate) mod tests {
         let (endpoint, _server) = start("0.0.0.0:0", mock).await?;
         let transport = Arc::new(test_transport(endpoint).await?);
 
-        let Runner { req_tx, _handle } = Runner::new(transport);
+        let Runner { req_tx, handle } = Runner::new(transport);
 
         let (resp_tx, resp_rx) = oneshot::channel();
         let write = WriteRequest {
@@ -253,7 +254,7 @@ pub(crate) mod tests {
         assert_eq!(status.message, "fail");
 
         drop(req_tx);
-        _handle.await?;
+        handle.await?;
 
         Ok(())
     }
@@ -267,7 +268,7 @@ pub(crate) mod tests {
         let (endpoint, _server) = start("0.0.0.0:0", mock).await?;
         let transport = Arc::new(test_transport(endpoint).await?);
 
-        let Runner { req_tx, _handle } = Runner::new(transport);
+        let Runner { req_tx, handle } = Runner::new(transport);
 
         // write 1
         let (resp_tx1, resp_rx1) = oneshot::channel();
@@ -317,7 +318,7 @@ pub(crate) mod tests {
 
         drop(req_tx);
         drop(response_tx);
-        _handle.await?;
+        handle.await?;
 
         Ok(())
     }
@@ -331,7 +332,7 @@ pub(crate) mod tests {
         let (endpoint, _server) = start("0.0.0.0:0", mock).await?;
         let transport = Arc::new(test_transport(endpoint).await?);
 
-        let Runner { req_tx, _handle } = Runner::new(transport);
+        let Runner { req_tx, handle } = Runner::new(transport);
 
         // write 1
         let (resp_tx1, resp_rx1) = oneshot::channel();
@@ -376,7 +377,7 @@ pub(crate) mod tests {
         assert_eq!(resp3, test_response(3));
 
         drop(response_tx);
-        _handle.await?;
+        handle.await?;
 
         Ok(())
     }
@@ -392,7 +393,7 @@ pub(crate) mod tests {
         let (endpoint, _server) = start("0.0.0.0:0", mock).await?;
         let transport = Arc::new(test_transport(endpoint).await?);
 
-        let Runner { req_tx, _handle } = Runner::new(transport);
+        let Runner { req_tx, handle } = Runner::new(transport);
 
         // write 1
         let (resp_tx1, resp_rx1) = oneshot::channel();
@@ -431,7 +432,7 @@ pub(crate) mod tests {
 
         // resp 3 - channel closed error
         let _resp3 = resp_rx3.await.expect_err("channel should be closed");
-        _handle.await?;
+        handle.await?;
 
         Ok(())
     }
