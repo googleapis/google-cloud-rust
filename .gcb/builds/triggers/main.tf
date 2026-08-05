774488f14c4e485087521983771fd4fcaaa80d38
Alvaro Viebrantz <aviebrantz@google.com>
2026-08-05 19:42:51 +0000

    chore: bump MSRV to 1.90

--- .gcb/builds/triggers/main.tf
+++ .gcb/builds/triggers/main.tf
@@ -138,10 +138,8 @@ locals {
       script = "test"
     }
     test-msrv = {
-      config       = "complex.yaml"
-      flags        = local.unstable_flags
-      script       = "test"
-      rust_version = "1.88"
+      config = "msrv.yaml"
+      flags  = local.unstable_flags
     }
     test-unstable-cfg = {
       config = "complex.yaml"
