469905c71b4aaa71f6284f3fa8786250a075a06e
Carlos O'Ryan <coryan@google.com>
2026-08-13 12:51:05 -0400

    docs(gax): fix `with_custom_header()` sample
    The sample was marked `ignore`. The generated uses `ignore` to speed up
    the PRs. One of the CI builds compiles post-merge.

--- src/gax/src/options.rs
+++ src/gax/src/options.rs
@@ -331,10 +331,14 @@ pub trait RequestOptionsBuilder: internal::RequestBuilder {
     /// comma-separated [`HeaderValue`]:
     ///
     /// ```rust,ignore
+    /// # use google_cloud_gax::options::RequestOptionsBuilder;
+    /// use http::header::{HeaderName, HeaderValue};
+    /// fn sample<T: RequestOptionsBuilder>(builder: T) -> T {
     /// builder.with_custom_header(
     ///     HeaderName::from_static("cache-control"),
     ///     HeaderValue::from_static("no-cache, no-store"),
     /// )
+    /// }
     /// ```
     ///
     /// [`HeaderValue`]: http::header::HeaderValue
