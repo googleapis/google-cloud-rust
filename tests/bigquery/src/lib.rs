8a4d6e9768db7b31ee33434867bb65e53c9f7969
haphungw <haphung@google.com>
2026-08-14 19:40:28 +0000

    test(bigquery): add long-running heavy poller test

--- tests/bigquery/src/lib.rs
+++ tests/bigquery/src/lib.rs
@@ -19,7 +19,9 @@ mod reads;
 mod writes;
 
 pub use dataset::dataset_admin;
-pub use job::{job_service, job_service_poller, job_service_poller_error};
+pub use job::{
+    job_service, job_service_poller, job_service_poller_error, job_service_poller_heavy,
+};
 pub use query::{
     query_client, query_client_datatypes, query_client_job, query_client_multi_page,
     query_client_nested_types, query_client_numeric_limits,
