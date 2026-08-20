7145ed6c0009b0e9e785c10deff2ddb3b1d5d472
Olivia Xiaoni Lai <5503815+xlai20@users.noreply.github.com>
2026-08-20 08:54:39 +0000

    chore(gax-internal): relocate Extensions re-export above ClientConfig doc comment in options.rs

--- src/gax-internal/src/options.rs
+++ src/gax-internal/src/options.rs
@@ -13,9 +13,9 @@
 // limitations under the License.
 
 pub use google_cloud_auth::credentials::Credentials;
+pub use google_cloud_gax::client_builder::internal::Extensions;
 
 // The client configuration for [crate::http::ReqwestClient] and [crate::grpc::Client].
-pub use google_cloud_gax::client_builder::internal::Extensions;
 pub type ClientConfig = google_cloud_gax::client_builder::internal::ClientConfig<Credentials>;
 
 pub(crate) const LOGGING_VAR: &str = "GOOGLE_CLOUD_RUST_LOGGING";
