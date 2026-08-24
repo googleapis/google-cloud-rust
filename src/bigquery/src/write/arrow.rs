d345562b979e744d0e757654fc5b53edec54e620
haphungw <haphung@google.com>
2026-08-24 23:05:39 +0000

    fmt

--- src/bigquery/src/write/arrow.rs
+++ src/bigquery/src/write/arrow.rs
@@ -12,10 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+mod base;
 mod buffered;
 mod committed;
 mod default;
-mod base;
 mod pending;
 mod writer_builder;
 
--- src/bigquery/src/write/arrow/base.rs
+++ src/bigquery/src/write/arrow/base.rs
@@ -21,7 +21,7 @@ use crate::write::transport::Transport;
 use std::sync::Arc;
 
 /// A shared internal structure for holding common state across different stream types.
-/// Providing shared implementations of operations core to most write streams. 
+/// Providing shared implementations of operations core to most write streams.
 /// Specific stream behaviors should be handled individually by their respective wrapper structs (e.g. `BufferedWriter`, `CommittedWriter`, `PendingWriter`).
 #[derive(Debug)]
 pub(crate) struct BaseWriter {
