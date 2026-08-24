3a90a2685a54fbf6be7e3ce226145a312e7fc0ea
Darren Bolduc <dbolduc@google.com>
2026-08-24 00:48:48 +0000

    rebase; d'oh

--- src/bigquery/examples/src/write/arrow_default.rs
+++ src/bigquery/examples/src/write/arrow_default.rs
@@ -19,7 +19,7 @@ use arrow::datatypes::{DataType, Field, Schema};
 use arrow::ipc::writer::StreamWriter;
 use arrow::record_batch::RecordBatch;
 use google_cloud_bigquery::client::Write;
-use google_cloud_bigquery::write::model::{ArrowRecordBatch, ArrowSchema};
+use google_cloud_bigquery::model::{ArrowRecordBatch, ArrowSchema};
 use std::sync::Arc;
 use tokio::task::JoinSet;
 
