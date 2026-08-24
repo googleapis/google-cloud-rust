57a3bbb8f7a2cd24dcb98ab250520dad865a7bd3
haphungw <haphung@google.com>
2026-08-24 21:53:55 +0000

    fmt

--- src/bigquery/src/write/arrow.rs
+++ src/bigquery/src/write/arrow.rs
@@ -12,10 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod inner;
 mod buffered;
 mod committed;
 mod default;
+mod inner;
 mod pending;
 mod writer_builder;
 
