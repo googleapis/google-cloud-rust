591bf01bdbdf7e80648d5f759829f1ed17bd5026
haphungw <haphung@google.com>
2026-08-25 00:54:00 +0000

    remove duplicate doc

--- src/bigquery/src/write/arrow/committed.rs
+++ src/bigquery/src/write/arrow/committed.rs
@@ -34,7 +34,6 @@ impl CommittedWriter {
         }
     }
 
-    /// Append rows to the stream.
     /// Return the full resource name of the underlying write stream.
     pub fn write_stream(&self) -> &str {
         &self.inner.write_stream
