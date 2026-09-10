80905069c16ae526d80afbf6796b31d041bfae4c
Darren Bolduc <dbolduc@google.com>
2026-09-10 06:02:47 +0000

    cleanup(bigquery)!: rename custom mod to `job_poller`

--- src/generated/cloud/bigquery/v2/src/lib.rs
+++ src/generated/cloud/bigquery/v2/src/lib.rs
@@ -114,4 +114,4 @@ pub(crate) use google_cloud_gax::options::internal::RequestBuilder;
 pub(crate) use google_cloud_gax::response::Response;
 
 #[allow(missing_docs)]
-pub mod operation;
+pub mod job_poller;
