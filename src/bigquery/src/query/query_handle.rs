e6a840756f2b9400750c59766e6a9096fa5d6ae7
Alvaro Viebrantz <aviebrantz@google.com>
2026-09-21 19:01:25 +0000

    refactor(bigquery): avoid cloning RetryContext when job is complete

--- src/bigquery/src/query/query_handle.rs
+++ src/bigquery/src/query/query_handle.rs
@@ -65,7 +65,7 @@ impl Query {
     pub(crate) fn from_job(
         job_service: Arc<JobService>,
         initial_job: Job,
-        retry_context: Option<RetryContext>,
+        retry_context: Option<&RetryContext>,
         page_size: Option<u32>,
     ) -> Self {
         let completed = initial_job
@@ -78,7 +78,11 @@ impl Query {
             completed,
             cached_rows: None,
             metadata: QueryMetadata::from(initial_job),
-            retry_context,
+            retry_context: if completed {
+                None
+            } else {
+                retry_context.cloned()
+            },
             page_size,
         }
     }
@@ -86,7 +90,7 @@ impl Query {
     pub(crate) fn from_query_response(
         job_service: Arc<JobService>,
         mut query_response: QueryResponse,
-        retry_context: Option<RetryContext>,
+        retry_context: Option<&RetryContext>,
         page_size: Option<u32>,
     ) -> Self {
         let completed = query_response.job_complete.unwrap_or(false);
@@ -97,7 +101,11 @@ impl Query {
             completed,
             cached_rows: Some(cached_rows),
             metadata,
-            retry_context,
+            retry_context: if completed {
+                None
+            } else {
+                retry_context.cloned()
+            },
             page_size,
         }
     }
@@ -814,7 +822,7 @@ mod tests {
         let mut retry_context = RetryContext::new(query_builder);
         retry_context.state.attempt_count = 1;
 
-        let query = Query::from_query_response(job_service, query_res, Some(retry_context), None);
+        let query = Query::from_query_response(job_service, query_res, Some(&retry_context), None);
 
         let completed = query.until_done().await?;
         assert_eq!(
@@ -890,7 +898,7 @@ mod tests {
         let mut retry_context = RetryContext::new(query_builder);
         retry_context.state.attempt_count = 1;
 
-        let query = Query::from_query_response(job_service, query_res, Some(retry_context), None);
+        let query = Query::from_query_response(job_service, query_res, Some(&retry_context), None);
 
         let err = query.until_done().await.unwrap_err();
         let errors = match err {
